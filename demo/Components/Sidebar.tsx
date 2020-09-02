10cc1c4a685db7ae94fcae2328d0f82abbf2a1d1
Matt Meigs <mattmeigs@gmail.com>
2020-09-01 21:58:47 -0400

    styled some with bootstrap and implemented very simple hooks

--- demo/Components/Sidebar.tsx
+++ demo/Components/Sidebar.tsx
@@ -6,26 +6,32 @@ declare global {
       button: any;
       div: any;
       h2: any;
-      h4: any;
+      h5: any;
       p: any;
     }
   }
 }
 
 const Sidebar = (props: any) => {
+  if (!Object.values(props.info).length) return (
+    <div className='sidebar-wrapper' style={{ border: '4px solid gray', borderRadius: '4px', padding: '10px', margin: '5px', height: '300px' }}> 
+    Choose a book!
+    </div>
+  );
+
   const { title, author, description, coverPrice, publicationDate, publisher } = props.info;
 
   return (
     <div className='sidebar-wrapper' style={{ border: '4px solid gray', borderRadius: '4px', padding: '10px', margin: '5px' }}> 
       <h2 style={{ textAlign: 'center' }}>{title}</h2>
-      <h4 style={{ textAlign: 'center' }}>{author}</h4>
+      <h5 style={{ textAlign: 'center' }}>- {author} -</h5>
 
-      <div className='priceDiv' style={{ border: '2px solid black', borderRadius: '4px', width: 'auto', display: 'flex', justifyContent: 'center' }}>
-        <p className='price' style={{ alignText: 'center' }}>${coverPrice}</p>
+      <div className='priceDiv' style={{ border: '2px solid black', borderRadius: '4px', width: 'auto', display: 'flex', justifyContent: 'center', alignItems: 'center', margin: '15px' }}>
+        <p className='price' style={{ padding: '5px 0px', textAlign: 'center', margin: '0' }}>${coverPrice}</p>
       </div>
 
       <div className='infoDiv' style={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-        <p style={{ justifyContent: 'center' }}>{description}</p>
+        <p style={{ justifyContent: 'center', textAlign: 'center' }}>{description}</p>
         <p style={{ marginBottom: '0px' }}>{publisher}</p>
         <p>{publicationDate}</p>
       </div>
--- demo/app.tsx
+++ demo/app.tsx
@@ -14,8 +14,31 @@ declare global {
   }
 }
 
+const book1 = {
+  title: 'Home on the Strange',
+  author: 'George R.R. Martin',
+  description: 'A cow freak show gets zapped into another dimension. Horror, mayhem, ozone. You never know what will happen next!',
+  coverPrice: 26.99,
+  publicationDate: 'June 15, 2025',
+  publisher: 'Liz Lotto Publishing'
+}
+
+const book2 = {
+  title: 'Let\'s Go!',
+  author: 'Jeho',
+  description: 'There has never been a motivator like this motivator.',
+  coverPrice: 4.99,
+  publicationDate: 'December 4, 1990',
+  publisher: 'Liz Lotto Publishing'
+}
+
 const App = () => {
-  // const [count, setCount] = (React as any).useState(0);
+  const [info, setInfo] = (React as any).useState({});
+  const [books, setBooks] = (React as any).useState([book1, book2]);
+
+  // (React as any).useEffect(() => {
+  //   setInfo(book2);
+  // })
 
   // return (
   //   <div>
@@ -25,15 +48,6 @@ const App = () => {
   //   </div>
   // );
 
-  const info = {
-    title: 'Home on the Strange',
-    author: 'George R.R. Martin',
-    description: 'A cow freak show gets zapped into another dimension. Horror, mayhem, ozone. You never know what will happen next!',
-    coverPrice: 26.99,
-    publicationDate: 'June 15, 2025',
-    publisher: 'Liz Lotto Publishing'
-  }
-
   return(
   <div>
   <h1 style={{ textAlign: 'center' }}>Burak's Book Bonanza</h1>
@@ -41,6 +55,8 @@ const App = () => {
     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
       <div style={{ height: '500px', width: '75%' }}>
         Book Carousel...
+        <button onClick={() => setInfo(books[0])}>Book 1</button>
+        <button onClick={() => setInfo(books[1])}>Book 2</button>
       </div>
 
       <div style={{ display: 'flex', minWidth: '200px', width: '25%', flexDirection: 'column', justifyContent: 'center' }}>
@@ -50,7 +66,13 @@ const App = () => {
 
     <div style={{ display: 'flex', justifyContent: 'space-between' }}>
       <BookNavBar />
-      <div style={{ marginRight: '5%' }}>Shopping Cart</div>
+      <div style={{ marginRight: '10%' }}>
+        <button type="button" className="btn btn-outline-primary">
+          <svg width="3em" height="3em" viewBox="0 0 16 16" className="bi bi-cart3" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+            <path fillRule="evenodd" d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84 4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2zm7 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2z"/>
+          </svg>
+        </button>
+      </div>
     </div>
   </div>
 </div>
