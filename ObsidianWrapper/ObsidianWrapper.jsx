8450466158c484d55240dbac20c0d170eb92cd76
Alonso Garza <alonsogarza@Alonsos-MacBook-Pro.local>
2020-09-07 11:28:09 -0500

    Adding wrapper.

---
+++ ObsidianWrapper/ObsidianWrapper.jsx
@@ -0,0 +1,49 @@
+import React from 'https://dev.jspm.io/react@16.13.1';
+
+export const TodoContext = React.createContext();
+
+const CACHE = {};
+
+function ObsidianWrapper(props) {
+  const [cache, setCache] = React.useState(CACHE);
+
+  function updateCache(query, response) {
+    setCache({ ...cache, [query]: response });
+  }
+  async function fetcher(query, endpoint = '/graphql') {
+    if (cache[query]) {
+      console.log('checking cache');
+      return new Promise((resolve, reject) => resolve(cache[query]));
+    } else {
+      console.log('fetching data');
+      try {
+        const respJSON = await fetch(endpoint, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+          body: JSON.stringify({ query }),
+        });
+        const resp = await respJSON.json();
+        updateCache(query, resp.data);
+
+        return resp.data;
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }
+  React.useEffect(() => {
+    console.log('CACHE:', cache);
+  }, [cache]);
+  const todoData = { cache, fetcher };
+
+  return <TodoContext.Provider value={todoData} {...props} />;
+}
+
+function useObsidian() {
+  return React.useContext(TodoContext);
+}
+
+export { ObsidianWrapper, useObsidian };
--- src/cache.js
+++ src/cache.js
@@ -4,27 +4,20 @@ const redis = await connect({ hostname: "127.0.0.1", port: 6379 });
 
 
 
-const BAO = {};
+
 
 export const checkCache = async query => {
-  console.log('QUERYYY', query)
   const response = await redis.get(query);
-  console.log('RESPONSE', response);
 
   return (response) ? JSON.parse(response) : false;
-  // if (BAO[query]) {
-  //   return JSON.parse(BAO[query]);
-  // } else {
-  //   return false;
-  // }
+
 }
 
 export const storeCache = (query, result) => {
 
 
-  console.log('-----PRE-----')
+  console.log('-----PRE-STORAGE-----')
   redis.setex(query, 15, JSON.stringify(result))
-  // BAO[query] = JSON.stringify(result);
-  console.log('-----POST-----')
+  console.log('-----POST-STORAGE-----')
 
 }
