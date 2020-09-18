a6429dec09f2a6f47d9e41658a887d44ae4bcec1
Travis Frank <travis@travismfrank.com>
2020-09-18 12:02:12 -0400

    Updated client-side cache to use state

--- ObsidianWrapper/ObsidianWrapper.jsx
+++ ObsidianWrapper/ObsidianWrapper.jsx
@@ -1,82 +1,82 @@
-import React from 'https://dev.jspm.io/react@16.13.1';
-
-import clientStorage from './clientStorage.js';
-
+import React from 'https://dev.jspm.io/react@16.13.1'; // from deps
+// import clientStorage from 'https://deno.land/x/obsidian@v0.1.2/ObsidianWrapper/clientStorage.js';
 import normalizeResult from '../src/normalize.js';
 import destructureQueries from '../src/destructureQueries.js';
-
-
 // Context will be used to create a custom provider for the application
 export const cacheContext = React.createContext();
-
 // Declaration of custom Obsidian Wrapper
 function ObsidianWrapper(props) {
-
-  const [cache, setCache] = React.useState(clientStorage);
-
+  const [cache, setCache] = React.useState({});
   // Primary function, provides access to fetching and caching capabilities
   async function fetcher(query, options = {}) {
-    console.log('initial state', window.__INITIAL_STATE__.obsidianSchema)
-    const obsidianSchema = window.__INITIAL_STATE__.obsidianSchema;
-    // Desctructuring of optional parameters, default values are defined and may be over written
-    const { endpoint = '/graphql', pollInterval = null } = options;
 
-    /* COMMENT OUT THESE LINES FOR SERVER CACHE */
-    const obsidianReturn = await destructureQueries(query, obsidianSchema);
-    // // Conditional to check if query is stored in global cache
-    if (obsidianReturn) {
-      console.log('--------------');
-      console.log('Found it in the cache!!');
-      console.log('--------------');
-      // Returning cached response as a promise
-      return new Promise(
-        (resolve, reject) => resolve(obsidianReturn)
-        // This can be uncommeted to store cache in session storage
-        // resolve(JSON.parse(sessionStorage.getItem(query)))
-      );
+    console.log(cache);
+
+    // Desctructuring of optional parameters, default values are defined and may be over written
+    const {
+      endpoint = '/graphql',
+      pollInterval = null,
+      destructure = true,
+    } = options;
+    if (destructure) {
+      const obsidianSchema = window.__INITIAL_STATE__.obsidianSchema;
+      /* COMMENT OUT THESE LINES FOR SERVER CACHE */
+      const deepCache = Object.assign({}, cache);
+      const obsidianReturn = await destructureQueries(query, obsidianSchema, deepCache);
+      // // Conditional to check if query is stored in global cache
+      if (obsidianReturn) {
+        console.log('--------------');
+        console.log('Found it in the cache!!');
+        console.log('--------------');
+        // Returning cached response as a promise
+        return new Promise(
+          (resolve, reject) => resolve(obsidianReturn)
+          // This can be uncommeted to store cache in session storage
+          // resolve(JSON.parse(sessionStorage.getItem(query)))
+        );
+      }
+    } else {
+      if (cache[query]) {
+        console.log('--------------');
+        console.log('Found it in the cache!!');
+        console.log('--------------');
+        return new Promise((resolve, reject) => resolve(cache[query]));
+      }
     }
     // If not found in cache, query is excecuted
-    else {
-
-      /* COMMENT OUT THESE LINES FOR SERVER CACHE */
-
-      // Conditional check, if poll interval has been defined
-      if (pollInterval) {
-        console.log(
-          `Setting ${
-            pollInterval / 1000
-          } second poll interval for graphql request`
-        );
-        // Initiation of reocurring fetch request
-        setInterval(() => {
-          console.log('--------------');
-          console.log('Fetching query with poll interval');
-          fetchData(query, endpoint);
-        }, pollInterval);
-      }
-      console.log('--------------');
-      console.log('Fetching Data');
-      // Excection of fetch
-      return await fetchData(query, endpoint);
     /* COMMENT OUT THESE LINES FOR SERVER CACHE */
+    // Conditional check, if poll interval has been defined
+    if (pollInterval) {
+      console.log(
+        `Setting ${
+          pollInterval / 1000
+        } second poll interval for graphql request`
+      );
+      // Initiation of reocurring fetch request
+      setInterval(() => {
+        console.log('--------------');
+        console.log('Fetching query with poll interval');
+        fetchData(query, endpoint);
+      }, pollInterval);
     }
+    console.log('--------------');
+    console.log('Fetching Data');
+    // Excection of fetch
+    return await fetchData(query, endpoint, destructure);
+    /* COMMENT OUT THESE LINES FOR SERVER CACHE */
     /* COMMENT OUT THESE LINES FOR SERVER CACHE */
   }
   // Function to update the global cache with new response data
   function updateCache(query, response) {
-    console.log('BEFORE: ', cache);
     // Declaring new object with new data to store in cache
     const newObj = Object.assign(cache, { [query]: response });
     // React hook to update global cache object
     setCache(newObj);
-    console.log('AFTER: ', newObj);
-    console.log('CACHEEE: ', cache);
     // Can be uncommeted to store data in session storage
     // sessionStorage.setItem(query, JSON.stringify(response));
   }
   // Excecutes graphql fetch request
-  async function fetchData(query, endpoint) {
-    const obsidianSchema = window.__INITIAL_STATE__.obsidianSchema;
+  async function fetchData(query, endpoint, destructure) {
     try {
       const respJSON = await fetch(endpoint, {
         method: 'POST',
@@ -88,11 +88,22 @@ function ObsidianWrapper(props) {
       });
       const resp = await respJSON.json();
       // Excecute function to update the cache with new response
-
-      /* COMMENT OUT THESE LINES FOR SERVER CACHE */
-      normalizeResult(query, resp, obsidianSchema);
-      /* COMMENT OUT THESE LINES FOR SERVER CACHE */
-
+      if (destructure) {
+        const obsidianSchema = window.__INITIAL_STATE__.obsidianSchema;
+        /* COMMENT OUT THESE LINES FOR SERVER CACHE */
+        const deepCache = Object.assign({}, cache);
+        normalizeResult(query, resp, obsidianSchema, deepCache)
+        .then(updatedCache => {
+          for (let key in updatedCache) {
+            for (let hash in updatedCache[key]) {
+              updateCache(hash, updatedCache[key][hash]);
+            }
+          }
+        });
+        /* COMMENT OUT THESE LINES FOR SERVER CACHE */
+      } else {
+        updateCache(query, resp);
+      }
       return resp;
     } catch (e) {
       console.log(e);
--- src/normalize.js
+++ src/normalize.js
@@ -2,22 +2,27 @@ import specificQueryParser from './specificQueryParser.js'
 import { checkAndInsert } from './dbOps.js';
 
 // Normalizes responses from GraphQL queries //
-export default function normalizeResult(query, result, obsidianSchema) {
+export default async function normalizeResult(query, result, obsidianSchema, cache) {
 
   const { returnTypes, obsidianTypeSchema } = obsidianSchema;
 
   const specificQueryArray = Object.keys(result.data);
 
+  const promiseArr = [];
+
   // Iterates through sub-queries to create hash-value pairs in Redis //
   for (let i = 0; i < specificQueryArray.length; i++) {
-    const hashedQuery = hashSpecificQuery(specificQueryArray[i], result.data[specificQueryArray[i]], returnTypes, query, obsidianTypeSchema);
-    checkAndInsert(hashedQuery.hash, hashedQuery.value);
+    const hashedQuery = await hashSpecificQuery(specificQueryArray[i], result.data[specificQueryArray[i]], returnTypes, query, obsidianTypeSchema, cache);
+    console.log(hashedQuery.value);
+    promiseArr.push(checkAndInsert(hashedQuery.hash, hashedQuery.value, cache));
   }
+
+  return Promise.all(promiseArr);
 }
 
 // Builds hash-value pair for each sub-query //
 // where hash = minified query && value = array of hashes for each property needed for the response //
-function hashSpecificQuery(queryType, fields, returnTypes, query, obsidianTypeSchema) {
+async function hashSpecificQuery(queryType, fields, returnTypes, query, obsidianTypeSchema, cache) {
 
   // Stringify the query to reveal newline characters
   query = JSON.stringify(query);
@@ -29,7 +34,7 @@ function hashSpecificQuery(queryType, fields, returnTypes, query, obsidianTypeSc
   const hash = specificQueryParser(startIdx, query).output;
 
   // Create array of hashes of all key:value pairs (will check and store in cache inside)
-  const objOfHashes = hashAndStoreFields(queryType, fields, returnTypes, obsidianTypeSchema);
+  const objOfHashes = await hashAndStoreFields(queryType, fields, returnTypes, obsidianTypeSchema, cache);
 
   return {
     hash: hash,
@@ -38,78 +43,82 @@ function hashSpecificQuery(queryType, fields, returnTypes, query, obsidianTypeSc
 }
 
 // Switch to deal with ListType versus NamedType //
-function hashAndStoreFields(queryType, fields, returnTypes, obsidianTypeSchema, typeSchemaName) {
+async function hashAndStoreFields(queryType, fields, returnTypes, obsidianTypeSchema, cache, typeSchemaName) {
   // Needed for nested Types //
   if (!typeSchemaName) typeSchemaName = returnTypes[queryType].type;
 
   // Iterate through ListType to hash and store each object //
   if (Array.isArray(fields)) {
     let output = {};
-    fields.forEach(el => {
-      output = Object.assign(output, hashAndStoreFieldsOfObject(typeSchemaName, el, obsidianTypeSchema, queryType, returnTypes));
-    })
+    for (let i = 0; i < fields.length; i++) {
+      output = Object.assign(output, await hashAndStoreFieldsOfObject(typeSchemaName, fields[i], obsidianTypeSchema, queryType, returnTypes, cache));
+    }
     return output;
   }
   // Hash and store NamedType //
-  return hashAndStoreFieldsOfObject(typeSchemaName, fields, obsidianTypeSchema, queryType, returnTypes);
+  return await hashAndStoreFieldsOfObject(typeSchemaName, fields, obsidianTypeSchema, queryType, returnTypes, cache);
 }
 
 // Create hash object and Store hash-value pairs in Redis of individual fields //
-function hashAndStoreFieldsOfObject(typeSchemaName, fields, obsidianTypeSchema, queryType, returnTypes) {
+async function hashAndStoreFieldsOfObject(typeSchemaName, fields, obsidianTypeSchema, queryType, returnTypes, cache) {
   const properties = Object.keys(fields);
   const id = (fields.id || fields.ID || fields._id || fields._ID || fields.Id || fields._Id);
 
   // Loop through properties to create object with all hashes //
-  const hashes = properties.reduce((acc, property) => {
-    // Only store non-id properties //
-    if (!property.toLowerCase().match(/_?id/)) {
-      const hash = hashGenerator(typeSchemaName, id, property);
-      let value;
+  const hashes = {};
 
-      // Check if the property is complex using our schema //
-      if (!obsidianTypeSchema[typeSchemaName][property].scalar) {
-        // Finds name of nested Type //
-        const nestedSchemaName = obsidianTypeSchema[typeSchemaName][property].type;
+  for (let i = 0; i < properties.length; i++) {
+    const oldReduce = async (property) => {
+      // Only store non-id properties //
+      if (!property.toLowerCase().match(/_?id/)) {
+        const hash = hashGenerator(typeSchemaName, id, property);
+        let value;
 
-        // Recursively hashes and stores properties within this property //
-        hashAndStoreFields(queryType, fields[property], returnTypes, obsidianTypeSchema, nestedSchemaName);
+        // Check if the property is complex using our schema //
+        if (!obsidianTypeSchema[typeSchemaName][property].scalar) {
+          // Finds name of nested Type //
+          const nestedSchemaName = obsidianTypeSchema[typeSchemaName][property].type;
 
-        // Check for ListType //
-        if (Array.isArray(fields[property])) {
+          // Recursively hashes and stores properties within this property //
+          await hashAndStoreFields(queryType, fields[property], returnTypes, obsidianTypeSchema, cache, nestedSchemaName);
 
-          value = {};
+          // Check for ListType //
+          if (Array.isArray(fields[property])) {
 
-          // Create hash prefixes //
-          for (let i = 0; i < fields[property].length; i++) {
-            const newId = checkID(fields[property][i])
+            value = {};
 
+            // Create hash prefixes //
+            for (let i = 0; i < fields[property].length; i++) {
+              const newId = checkID(fields[property][i])
+
+              if (newId) {
+                value[nestedSchemaName + '~' + newId] = true;
+              } else {
+                value = null;
+              }
+            }
+            // NamedType
+          } else {
+            const newId = checkID(fields[property])
             if (newId) {
-              value[nestedSchemaName + '~' + newId] = true;
+              value = JSON.stringify(nestedSchemaName + '~' + newId);
             } else {
               value = null;
             }
           }
-          // NamedType 
         } else {
-          const newId = checkID(fields[property])
-          if (newId) {
-            value = JSON.stringify(nestedSchemaName + '~' + newId);
-          } else {
-            value = null;
-          }
+          // Property is scalar //
+          value = fields[property];
         }
-      } else {
-        // Property is scalar //
-        value = fields[property];
-      }
-      // Store hash key and value in Redis database //
-      checkAndInsert(hash, value);
+        // Store hash key and value in Redis database //
+        await checkAndInsert(hash, value, cache);
 
-      // Add hash to hash object //
-      acc[hash] = true;
+        // Add hash to hash object //
+        hashes[hash] = true;
+      }
     }
-    return acc;
-  }, {})
+    await oldReduce(properties[i]);
+  }
 
   return hashes;
 }
@@ -133,7 +142,7 @@ function checkID(propObj) {
       // Undefined is not valid. Developer did not return id field. //
       throw new Error('Must return id field with each NamedType in your query');
     }
-  } 
+  }
 
   return newId;
 }
