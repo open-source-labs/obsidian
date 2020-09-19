aa2d909d1b38a21e72b7d10b51b6401b33b8877a
Matt Meigs <mattmeigs@gmail.com>
2020-09-19 16:07:49 -0400

    cleaned up client side null problem for good

--- src/destructureQueries.js
+++ src/destructureQueries.js
@@ -65,8 +65,8 @@ async function buildResultsObject(hashes, obsidianSchema, queryObj, cache) {
         propVal = JSON.parse(await retrieveScalar(hashes[j], cache));
       } else {
         propVal = await retrieveScalar(hashes[j], cache);
-        if (propVal.slice(1, 5) === 'null') propVal = null;
-      }
+        if (propVal && propVal.slice(1, 5) === 'null') propVal = null;
+      } 
       queryResult[id][property] = propVal;
       // If this field is complex, recursive call to build nested property object //
     } else {
