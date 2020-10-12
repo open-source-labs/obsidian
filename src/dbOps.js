0865fd4faaaf334a8ad1630431eb8a2d1a5f349d
Travis Frank <travis@travismfrank.com>
2020-10-12 18:23:19 -0400

    Testing Redis config

--- src/dbOps.js
+++ src/dbOps.js
@@ -65,6 +65,12 @@ async function checkAndRetrieveQuery(hash, cache) {
   if (browser) {
     return cache[hash];
   } else {
+    if (!redis) {
+      import('./cache.js')
+      .then(mod => {
+        connectFunc = mod.default;
+      })
+    }
     if (!redis || redis.isClosed) redis = await connectFunc(browser);
     let ifCached = await redis.get(hash);
     if (ifCached) ifCached = JSON.parse(ifCached);
