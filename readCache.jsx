079d58eb5a88e7cf8c90be1decb662e972cfd03f
Esma-Sahraoui <esma.sahraoui@gmail.com>
2020-12-12 09:23:58 -0500

    added comments and notes

--- readCache.jsx
+++ readCache.jsx
@@ -2,9 +2,19 @@
 /* eslint-disable guard-for-in */
 /**
  * NOTES:
- * 1.
+ * 1.This function will assume that everything passed in is a query, not a mutation
+ * 2. This function would assume that the input query passed in is the result from destructuring the original query.
+ * 3. This function accepts one/multiple queries as input
+ * 4. We won't worry about arguments on fields for now
+ * 5. We won't worry about aliases for now
+ * 6. We won't worry about handling directives for now
+ * 7. We wont' worry about fragments for now
+ * 8. We won't handle variables for now
+ * 9. We will handle only the meta field "__typename" for now
+ * 10. What edges cases can we worry about: 
+
  */
-const realCache = {
+const cacheObject = {
   ROOT_QUERY: {
     movies: ['Movie~1', 'Movie~2', 'Movie~3', 'Movie~4'],
     actors: ['Actor~1', 'Actor~2', 'Actor~3', 'Actor~4'],
@@ -43,27 +53,20 @@ const realCache = {
   'Actor~5': { id: '5', firstName: 'Gary' },
 };
 
-// const inputQueryTwo = [
-//   {
-//     name: 'movies',
-//     arguments: '',
-//     fields: ['id', 'title', { actors: ['id', 'firstName'] }],
-//   },
-// ];
-
 //* CRUD READ
 function readCache(queries, cache) {
-  const dataRes = {};
-  // iterate through each query in the queries object
+  const responseObject = {};
+  // iterate through each query in the input queries object
   for (const query in queries) {
-    // get the entire str query from the name query and arguments
+    // get the entire str query from the name input query and arguments
     const rootQuery = queries[query].name.concat(queries[query].arguments);
     // match in ROOT_QUERY
     if (cache.ROOT_QUERY[rootQuery]) {
+      // get the types to populate from the existent query in the cache
       const arrayTypes = cache.ROOT_QUERY[rootQuery];
 
-      // invoke populate and add data objects to the data response
-      dataRes[queries[query].name] = populate(
+      // invoke populateAllTypes and add data objects to the response object for each input query
+      responseObject[queries[query].name] = populateAllTypes(
         arrayTypes,
         cache,
         queries[query].fields
@@ -71,32 +74,40 @@ function readCache(queries, cache) {
       // no match with ROOT_QUERY return null or ...
     } else return 'query not found';
   }
-  return { data: dataRes };
+  return { data: responseObject };
 }
-//* helper function that populates dataRes types with fields
-function populate(arrTypes, cache, fields) {
-  // include the type name for each types
+//* helper function that populates responseObject types with fields
+function populateAllTypes(arrTypes, cache, fields) {
+  // include the typename for each type
   const hyphenIdx = arrTypes[0].indexOf('~');
   const typeName = arrTypes[0].slice(0, hyphenIdx);
-
-  return arrTypes.reduce((acc, type, idx) => {
-    acc.push({});
-    const dataObj = acc[idx];
+  return arrTypes.reduce((acc, type) => {
+    // for each type from the input query, build the response object
+    const dataObj = {};
     for (const field in fields) {
+      // for each field in the fields input query, add the corresponding value from the cache if the field is not another array of types
       if (typeof fields[field] !== 'object') {
+        // add the typename for the type
         if (field === '__typename') {
           dataObj[field] = typeName;
         } else dataObj[field] = cache[type][field];
       } else {
-        dataObj[field] = populate(cache[type][field], cache, fields[field]);
+        // case where the field from the input query is an array of types, recursively invoke populateAllTypes
+        dataObj[field] = populateAllTypes(
+          cache[type][field],
+          cache,
+          fields[field]
+        );
       }
     }
+    // acc is an array of response object for each type
+    acc.push(dataObj);
     return acc;
   }, []);
 }
 
 //* input after destructuring queries parser
-const inputQueryOne = [
+const inputQueries = [
   {
     name: 'movies',
     arguments: '(input:{genre:ACTION})',
@@ -181,8 +192,8 @@ const testResponse = {
     ],
   },
 };
-const response = readCache(inputQueryOne, realCache);
-console.log(readCache(inputQueryOne, realCache));
+const response = readCache(inputQueries, cacheObject);
+console.log(response);
 console.log(response.data);
 
 console.log(JSON.stringify(response) === JSON.stringify(testResponse));
