acfb824aef0ddca062317fb856199a03f3fbded1
Spencer Stockton <spencerstockton@gmail.com>
2020-12-12 16:41:21 -0500

    Updated test environment to be prettier compliant

--- test_files/rhum_test_files/normalize_test.ts
+++ test_files/rhum_test_files/normalize_test.ts
@@ -1,20 +1,20 @@
-import { Rhum } from "https://deno.land/x/rhum@v1.1.4/mod.ts";
+import { Rhum } from 'https://deno.land/x/rhum@v1.1.4/mod.ts';
 
-Rhum.testPlan("app_test.ts", () => {
-    Rhum.testSuite("normalizeTestSuite", () => {
-        Rhum.testCase("normalizeTestSuite test one", async () => {
-        const result = true;
-        Rhum.asserts.assertEquals(true, result);
-        });
-        Rhum.testCase("normalizeTestSuite test two", async () => {
-            const result = true;
-            Rhum.asserts.assertEquals(true, result);
-        });
-        Rhum.testCase("normalizeTestSuite test three", async () => {
-            const result = true;
-            Rhum.asserts.assertEquals(true, result);
-        });
+Rhum.testPlan('app_test.ts', () => {
+  Rhum.testSuite('normalizeTestSuite', () => {
+    Rhum.testCase('normalizeTestSuite test one', async () => {
+      const result = true;
+      Rhum.asserts.assertEquals(true, result);
     });
+    Rhum.testCase('normalizeTestSuite test two', async () => {
+      const result = true;
+      Rhum.asserts.assertEquals(true, result);
+    });
+    Rhum.testCase('normalizeTestSuite test three', async () => {
+      const result = true;
+      Rhum.asserts.assertEquals(true, result);
+    });
+  });
 });
 
 Rhum.run();
