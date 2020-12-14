05dde7ae360a18c66dacb0eb8fc4ee74eba94641
Esma-Sahraoui <esma.sahraoui@gmail.com>
2020-12-14 14:46:57 -0500

    started tests for createCache

--- src/createCache.js
+++ src/createCache.js
@@ -1,5 +1,5 @@
 /** NOTES:
- * 1.This function will assume that everything passed in is a response object from gql request, query object from the destructure queries, and the original cache
+ * 1.This function will assume that everything passed in is a response object from gql request, query string (use the destructure queries to return a query object), and the original cache
  * 2. This function will add new fields to the cache and new key/value pair queries to the ROOT_QUERY .
  * 3. This function will update an existent field or an existent ROOT_QUERY key/value pair.
  * 4. This function will return the updated cache with the same reference.
@@ -93,13 +93,13 @@ const originalCache = {
     films: ['Movie~1'],
   },
 };
-function creatCache(resFromNormalize, cache) {
+export function createCache(resFromNormalize, cache) {
   // update the root query from both objects and merge them into the response object. This will modify resFromNormalize with the updated ROOT_QUERY
   Object.assign(resFromNormalize.ROOT_QUERY, cache.ROOT_QUERY);
   // update the original cache with the response object. This will return the reference to the original cache
   return Object.assign(cache, resFromNormalize);
 }
 
-const respCache = creatCache(toAddInCache, originalCache);
+const respCache = createCache(toAddInCache, originalCache);
 console.log(respCache);
 console.log(originalCache === respCache);
---
+++ test_files/rhum_test_files/createCache_test.ts
@@ -0,0 +1,181 @@
+/**
+ * NOTES:
+ * 1.This file will test readCache functionality:
+ * Should return an object if all values are in the cache.
+ * Should return undefined if any field is missing value  in the cache.
+ * Should accept multiple queries in one query operation.
+ * 2. This file will test populateAllTypes functionality:
+ * Should return an array if all fields are found.
+ * Should return undefined if any field is missing value.
+ * Reviews:
+ * Change comments for what we are testing
+ * Adding a file that contains all the variables for the test suite readCache
+ */
+
+import { createCache } from '../../src/createCache.js';
+import { Rhum } from 'https://deno.land/x/rhum@v1.1.4/mod.ts';
+const toAddInCache = {
+  ROOT_QUERY: {
+    'actor(id:1)': 'Actor~1',
+    movies: ['Movie~1', 'Movie~2', 'Movie~3', 'Movie~4'],
+  },
+  'Movie~1': {
+    id: '1',
+    title: 'Indiana Jones and the Last Crusade',
+    actors: ['Actor~1', 'Actor~2'],
+    genre: 'ACTION',
+    releaseYear: 1989,
+  },
+  'Movie~2': {
+    id: '2',
+    title: 'Empire Strikes Back',
+    actors: ['Actor~1', 'Actor~3'],
+    releaseYear: 1980,
+  },
+  'Movie~3': {
+    id: '3',
+    title: 'Witness',
+    actors: ['Actor~1', 'Actor~4'],
+    releaseYear: 1985,
+  },
+  'Movie~4': {
+    id: '4',
+    title: 'Air Force One',
+    actors: ['Actor~1', 'Actor~5'],
+    genre: 'ACTION',
+    releaseYear: 1997,
+  },
+  'Actor~1': {
+    id: '1',
+    firstName: 'Harrison',
+    lastName: 'Ford',
+    films: ['Movie~1', 'Movie~2', 'Movie~3', 'Movie~4'],
+  },
+  'Actor~3': {
+    id: '3',
+    firstName: 'Mark',
+    lastName: 'Hamill',
+    films: ['Movie~2'],
+  },
+  'Actor~4': {
+    id: '4',
+    firstName: 'Patti',
+    lastName: 'LuPone',
+    films: ['Movie~3'],
+  },
+  'Actor~5': {
+    id: '5',
+    firstName: 'Gary',
+    lastName: 'Oldman',
+    films: ['Movie~4'],
+  },
+};
+const originalCache = {
+  ROOT_QUERY: {
+    'movies(input:{genre:ACTION})': ['Movie~1', 'Movie~4'],
+    actors: ['Actor~1', 'Actor~2', 'Actor~3', 'Actor~4', 'Actor~5'],
+  },
+  'Movie~1': {
+    id: '1',
+    title: 'Indiana Jones and the Last Crusade',
+    actors: ['Actor~1', 'Actor~2'],
+    genre: 'ACTION',
+  },
+  'Movie~2': {
+    id: '2',
+    title: 'Empire Strikes Back',
+  },
+  'Movie~3': {
+    id: '3',
+    title: 'Witness',
+  },
+  'Movie~4': {
+    id: '4',
+    title: 'Air Force One',
+    actors: ['Actor~1', 'Actor~5'],
+    genre: 'ACTION',
+  },
+
+  'Actor~2': {
+    id: '2',
+    firstName: 'Sean',
+    lastName: 'Connery',
+    films: ['Movie~1'],
+  },
+};
+const expectedResultCache = {
+  ROOT_QUERY: {
+    'actor(id:1)': 'Actor~1',
+    movies: ['Movie~1', 'Movie~2', 'Movie~3', 'Movie~4'],
+    'movies(input:{genre:ACTION})': ['Movie~1', 'Movie~4'],
+    actors: ['Actor~1', 'Actor~2', 'Actor~3', 'Actor~4', 'Actor~5'],
+  },
+  'Movie~1': {
+    id: '1',
+    title: 'Indiana Jones and the Last Crusade',
+    actors: ['Actor~1', 'Actor~2'],
+    genre: 'ACTION',
+    releaseYear: 1989,
+  },
+  'Movie~2': {
+    id: '2',
+    title: 'Empire Strikes Back',
+    actors: ['Actor~1', 'Actor~3'],
+    releaseYear: 1980,
+  },
+  'Movie~3': {
+    id: '3',
+    title: 'Witness',
+    actors: ['Actor~1', 'Actor~4'],
+    releaseYear: 1985,
+  },
+  'Movie~4': {
+    id: '4',
+    title: 'Air Force One',
+    actors: ['Actor~1', 'Actor~5'],
+    genre: 'ACTION',
+    releaseYear: 1997,
+  },
+  'Actor~1': {
+    id: '1',
+    firstName: 'Harrison',
+    lastName: 'Ford',
+    films: ['Movie~1', 'Movie~2', 'Movie~3', 'Movie~4'],
+  },
+  'Actor~2': {
+    id: '2',
+    firstName: 'Sean',
+    lastName: 'Connery',
+    films: ['Movie~1'],
+  },
+  'Actor~3': {
+    id: '3',
+    firstName: 'Mark',
+    lastName: 'Hamill',
+    films: ['Movie~2'],
+  },
+  'Actor~4': {
+    id: '4',
+    firstName: 'Patti',
+    lastName: 'LuPone',
+    films: ['Movie~3'],
+  },
+  'Actor~5': {
+    id: '5',
+    firstName: 'Gary',
+    lastName: 'Oldman',
+    films: ['Movie~4'],
+  },
+};
+Rhum.testPlan('createCache.js', () => {
+  Rhum.testSuite('creatCache()', () => {
+    Rhum.testCase(
+      'should return an updated cache with the new fields and queries',
+      () => {
+        const result = createCache(toAddInCache, originalCache);
+        Rhum.asserts.assertEquals(result, expectedResultCache);
+      }
+    );
+  });
+});
+// deno test --allow-env
