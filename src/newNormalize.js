69783557ed14998f37defc1b414ff663782f32ca
Eric  Marcatoma <ericmarcatoma@new-host-9.home>
2020-12-14 10:05:40 -0500

    updated normalize doc

--- src/newNormalize.js
+++ src/newNormalize.js
@@ -1,5 +1,6 @@
 /**
  * NOTES:
+ * 1.Assuming that all response objects will have an ID
  */
 //==============================================================================
 //INPUT:
@@ -7,27 +8,29 @@
 const queryObject = {
   queries: [
     {
-      name: "movies",
-      arguments: "(input:{genre:ACTION})",
+      __typename: 'meta',
+      name: 'movies',
+      arguments: '(input:{genre:ACTION})',
       fields: {
-        __typename: "meta",
-        id: "scalar",
-        title: "scalar",
-        genre: "scalar",
-        actors: { id: "scalar", firstName: "scalar", lastName: "scalar" },
+        __typename: 'meta',
+        id: 'scalar',
+        title: 'scalar',
+        genre: 'scalar',
+        actors: {id: 'scalar', firstName: 'scalar', lastName: 'scalar'},
       },
     },
     {
-      name: "actors",
-      arguments: "",
+      __typename: 'meta',
+      name: 'actors',
+      arguments: '',
       fields: {
-        id: "scalar",
-        firstName: "scalar",
-        lastName: "scalar",
+        id: 'scalar',
+        firstName: 'scalar',
+        lastName: 'scalar',
         films: {
-          __typename: "meta",
-          id: "scalar",
-          title: "scalar",
+          __typename: 'meta',
+          id: 'scalar',
+          title: 'scalar',
         },
       },
     },
@@ -38,107 +41,126 @@ const resultObject = {
   data: {
     movies: [
       {
-        __typename: "Movie",
-        id: "1",
-        title: "Indiana Jones and the Last Crusade",
-        genre: "ACTION",
-        actors: [{
-          __typename: "Actor",
-          id: "1",
-          firstName: "Harrison",
-          lastName: "Ford",
-        }, {
-          __typename: "Actor",
-          id: "2",
-          firstName: "Sean",
-          lastName: "Connery",
-        }],
+        __typename: 'Movie',
+        id: '1',
+        title: 'Indiana Jones and the Last Crusade',
+        genre: 'ACTION',
+        actors: [
+          {
+            __typename: 'Actor',
+            id: '1',
+            firstName: 'Harrison',
+            lastName: 'Ford',
+          },
+          {
+            __typename: 'Actor',
+            id: '2',
+            firstName: 'Sean',
+            lastName: 'Connery',
+          },
+        ],
       },
       {
-        __typename: "Movie",
-        id: "4",
-        title: "Air Force One",
-        genre: "ACTION",
-        actors: [{
-          __typename: "Actor",
-          id: "1",
-          firstName: "Harrison",
-          lastName: "Ford",
-        }, {
-          __typename: "Actor",
-          id: "5",
-          firstName: "Gary",
-          lastName: "Oldman",
-        }],
+        __typename: 'Movie',
+        id: '4',
+        title: 'Air Force One',
+        genre: 'ACTION',
+        actors: [
+          {
+            __typename: 'Actor',
+            id: '1',
+            firstName: 'Harrison',
+            lastName: 'Ford',
+          },
+          {
+            __typename: 'Actor',
+            id: '5',
+            firstName: 'Gary',
+            lastName: 'Oldman',
+          },
+        ],
       },
     ],
     actors: [
       {
-        __typename: "Actor",
-        id: "1",
-        firstName: "Harrison",
-        lastName: "Ford",
-        films: [{
-          __typename: "Movie",
-          id: "1",
-          title: "Indiana Jones and the Last Crusade",
-        }, {
-          __typename: "Movie",
-          id: "2",
-          title: "Empire Strikes Back",
-        }, {
-          __typename: "Movie",
-          id: "3",
-          title: "Witness",
-        }, {
-          __typename: "Movie",
-          id: "4",
-          title: "Air Force One",
-        }],
+        __typename: 'Actor',
+        id: '1',
+        firstName: 'Harrison',
+        lastName: 'Ford',
+        films: [
+          {
+            __typename: 'Movie',
+            id: '1',
+            title: 'Indiana Jones and the Last Crusade',
+          },
+          {
+            __typename: 'Movie',
+            id: '2',
+            title: 'Empire Strikes Back',
+          },
+          {
+            __typename: 'Movie',
+            id: '3',
+            title: 'Witness',
+          },
+          {
+            __typename: 'Movie',
+            id: '4',
+            title: 'Air Force One',
+          },
+        ],
       },
       {
-        __typename: "Actor",
-        id: "2",
-        firstName: "Sean",
-        lastName: "Connery",
-        films: [{
-          __typename: "Movie",
-          id: "1",
-          title: "Indian Jones and the Last Crusade",
-        }],
+        __typename: 'Actor',
+        id: '2',
+        firstName: 'Sean',
+        lastName: 'Connery',
+        films: [
+          {
+            __typename: 'Movie',
+            id: '1',
+            title: 'Indian Jones and the Last Crusade',
+          },
+        ],
       },
       {
-        __typename: "Actor",
-        id: "3",
-        firstName: "Mark",
-        lastName: "Hamill",
-        films: [{
-          __typename: "Movie",
-          id: "2",
-          title: "Empire Strikes Back",
-        }],
+        __typename: 'Actor',
+        id: '3',
+        firstName: 'Mark',
+        lastName: 'Hamill',
+        films: [
+          {
+            __typename: 'Movie',
+            id: '2',
+            title: 'Empire Strikes Back',
+          },
+        ],
       },
       {
-        __typename: "Actor",
-        id: "4",
-        firstName: "Patti",
-        lastName: "LuPone",
-        films: [{
-          __typename: "Movie",
-          id: "3",
-          title: "Witness",
-        }],
+        __typename: 'Actor',
+        id: '4',
+        firstName: 'Patti',
+        lastName: 'LuPone',
+        films: [
+          {
+            __typename: 'Movie',
+            id: '3',
+            title: 'Witness',
+          },
+        ],
       },
       {
-        __typename: "Actor",
-        id: "5",
-        firstName: "Gary",
-        lastName: "Oldman",
-        films: [{
-          __typename: "Movie",
-          id: "4",
-          title: "Air Force One",
-        }],
+        __typename: 'Actor',
+        id: '5',
+        firstName: 'Gary',
+        lastName: 'Oldman',
+        films: [
+          {
+            __typename: 'Movie',
+            id: '4',
+            title: 'Air Force One',
+          },
+        ],
       },
     ],
   },
@@ -148,9 +170,8 @@ const resultObject = {
 
 //Normalizes responses using the query object from destructure and the response object from
 //the graphql request
-export default async function normalizeResult(queryObj, resultObj) {
-  //
-}
+
+//
 
 //===========================================================
 //OUTPUT
@@ -159,48 +180,58 @@ const output = normalizeResult(queryObject, resultObject);
 
 //
 const resultObj = {
-  "movies(input:{genre:ACTION})": ["Movie~1", "Movie~4"],
-  "actors": ["Actor~1", "Actor~2", "Actor~3", "Actor~4", "Actor~5"],
-  "Movie~1": {
-    id: "1",
-    title: "Indiana Jones and the Last Crusade",
-    actors: ["Actor~1", "Actor~2"],
-    genre: "ACTION",
+  ROOT_QUERY: {
+    'movies(input:{genre:ACTION})': ['Movie~1', 'Movie~4'],
+    actors: ['Actor~1', 'Actor~2', 'Actor~3', 'Actor~4', 'Actor~5'],
   },
-  "Movie~4": {
-    id: "4",
-    title: "Air Force One",
-    actors: ["Actor~1", "Actor~5"],
-    genre: "ACTION",
+  'Movie~1': {
+    id: '1',
+    title: 'Indiana Jones and the Last Crusade',
+    actors: ['Actor~1', 'Actor~2'],
+    genre: 'ACTION',
   },
-  "Actor~1": {
-    id: "1",
-    firstName: "Harrison",
-    lastName: "Ford",
-    films: ["Movie~1", "Movie~2", "Movie~3", "Movie~4"],
+  'Movie:~2': {
+    id: '2',
+    title: 'Empire Strikes Back',
   },
-  "Actor~2": {
-    id: "2",
-    firstName: "Sean",
-    lastName: "Connery",
-    films: ["Movie~1"],
+  'Movie:~3': {
+    id: '3',
+    title: 'Witness',
   },
-  "Actor~3": {
-    id: "3",
-    firstName: "Mark",
-    lastName: "Hamill",
-    films: ["Movie~2"],
+  'Movie~4': {
+    id: '4',
+    title: 'Air Force One',
+    actors: ['Actor~1', 'Actor~5'],
+    genre: 'ACTION',
   },
-  "Actor~4": {
-    id: "4",
-    firstName: "Patti",
-    lastName: "LuPone",
-    films: ["Movie~3"],
+  'Actor~1': {
+    id: '1',
+    firstName: 'Harrison',
+    lastName: 'Ford',
+    films: ['Movie~1', 'Movie~2', 'Movie~3', 'Movie~4'],
   },
-  "Actor~5": {
-    id: "5",
-    firstName: "Gary",
-    lastName: "Oldman",
-    films: ["Movie~4"],
+  'Actor~2': {
+    id: '2',
+    firstName: 'Sean',
+    lastName: 'Connery',
+    films: ['Movie~1'],
+  },
+  'Actor~3': {
+    id: '3',
+    firstName: 'Mark',
+    lastName: 'Hamill',
+    films: ['Movie~2'],
+  },
+  'Actor~4': {
+    id: '4',
+    firstName: 'Patti',
+    lastName: 'LuPone',
+    films: ['Movie~3'],
+  },
+  'Actor~5': {
+    id: '5',
+    firstName: 'Gary',
+    lastName: 'Oldman',
+    films: ['Movie~4'],
   },
 };
