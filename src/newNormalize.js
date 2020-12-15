80f365f1c7731898efde0c8422830d7052492282
Eric  Marcatoma <ericmarcatoma@new-host-9.home>
2020-12-14 22:15:10 -0500

    normalize update

--- src/newNormalize.js
+++ src/newNormalize.js
@@ -214,102 +214,158 @@ const resultObject = {
 
 //==========================================================
 
-//Normalizes responses using the query object from destructure and the response object from
-//the graphql request
-export default async function normalizeResult(queryObj, resultObj) {
-  if (!resultObj.data) return;
+// Normalizes responses using the query object from destructure and the response object from
+// the graphql request
+export default function normalizeResult(queryObj, resultObj) {
+  // Object to hold normalized obj
+  const result = {};
+  // creates a stringified version of query request and stores it in ROOT_QUERY key
+  result['ROOT_QUERY'] = createRootQuery(queryObj, resultObj);
 
-  //Object to hold normalized obj
-  const cache = {};
-
-  //
   let resultKeys = Object.keys(resultObj.data);
   const hashes = [];
 
-  //creates a stringified version of query request and stores it in ROOT_QUERY key
-  cache['ROOT_QUERY'] = createRootQuery(queryObj, resultObj);
-
-  //stores appropriate response obj with hashed __typename and id
+  // stores appropriate response obj with hashed __typename and id
   for (let i = 0; i < resultKeys.length; i++) {
-    //curr assigned the value of the response of that query
+    // curr assigned the value of the response of that query
     const curr = resultObj.data[resultKeys[i]];
     for (let j = 0; j < curr.length; j++) {
-      //pass current obj to createHash function to create hash and appropriate response obj
-      const hash = createHash(curr[j]);
-      //store the output of createHash in output cache obj
-      if (cache[hash.hash]){
-        Object.assign(cache[hash.hash], hash.output)
-      } else {
-        cache[hash.hash] = hash.output
+      // pass current obj to createHash function to create hash and appropriate response obj
+      const hashObj = createHash(curr[j]);
+      // store the output of createHash in output cache obj
+      for (const hash in hashObj) {
+        if (result[hash]) {
+          Object.assign(result[hash], hashObj[hash])
+        } else{
+          result[hash] = hashObj[hash]
+        }
       }
-      console.log(cache);
-      
     }
   }
-  console.log(cache);
-  return cache;
+  console.log(result);
+  return result;
 }
 
-//checks if the object in innerObj exsits within output, if not create new reference in the output cache
-function checkAndInsert(innerObj, output) {
-  for (let obj in innerObj) {
-    if (!output[obj]) {
-      output[obj] = innerObj[obj];
-    } else {
-      Object.assign(output[obj], innerObj[obj]);
-    }
-  }
-}
+// checks if the object in innerObj exsits within output, if not create new reference in the output cache
 
-//creates the hashes for query requests and stores the references with the appropriate hashes in an object
+// creates the hashes for query requests and stores the references with the appropriate hashes in an object
 function createRootQuery(queryObj, resultObj) {
-  let output = {};
-  for (let i = 0; i < queryObj.queries.length; i++) {
-    const name = queryObj.queries[i].name;
-    const args = queryObj.queries[i].arguments;
+  const output = {};
+  queryObj.queries.forEach((query) => {
+    const name = query.name;
+    const args = query.arguments;
+    const queryHash = name + args;
     //create hashes that will be store the appropriate root query value
-    const hashes = createHash(resultObj.data).output;
-    console.log(hashes);
-
+    const resultArray = resultObj.data[name];
+    const arrOfHashes = [];
+    resultArray.forEach((obj) => {
+      arrOfHashes.push(obj.__typename + '~' + obj.id);
+    });
     //store the hashes associated with the query request and arguement
-    output[name + args] = hashes[name];
-  }
-  console.log(output);
+    output[queryHash] = arrOfHashes;
+  });
   return output;
 }
 
 //creates hashes and checks for complex fields
 //returns hashes, appropriate obj, and innerObj if a complex field exists
-function createHash(obj, output) {
-  let hash = obj.__typename + '~' + obj.id;
-  let output = {};
-
-  // let innerOutput = {};
-  for (let curr in obj) {
-    //checks if the current value is an array
-    if (!Array.isArray(obj[curr])) {
-      //if not array and the curr key isn't __typename add to output obj
-      if (curr !== '__typename') {
-        output[curr] = obj[curr];
-      }
-
-      //if array, we create hashes and appropriate obj for those hashes
+function createHash(obj, output = {}) {
+  const hash = obj.__typename + '~' + obj.id;
+  if (!output[hash]) output[hash] = {};
+  for (const field in obj) {
+    if (field === '__typename') continue;
+    if (!Array.isArray(obj[field])) {
+      output[hash][field] = obj[field];
     } else {
-      let nestedField = obj[curr];
-      const hashArr = [];
-      let hashObj = {};
-      for (let i = 0; i < nestedField.length; i++) {
-        let nestedHash = createHash(nestedField[i]);
-        console.log(nestedHash.output);
-        console.log(nestedHash.hash);
-        hashArr.push(nestedHash.hash);
-      }
-      output[curr] = hashArr;
+      // create an array of hashes
+      output[hash][field] = [];
+      obj[field].forEach((obj) => {
+        const arrayHash = obj.__typename + '~' + obj.id;
+        output[hash][field].push(arrayHash);
+        output = createHash(obj, output);
+      });
+      // store hashed values in output
     }
   }
-  console.log({hash, output});
-  return {hash, output};
+  return output;
 }
+
+// ================================
+
+const hashInput = {
+  __typename: 'Movie',
+  id: '1',
+  title: 'Indiana Jones and the Last Crusade',
+  genre: 'ACTION',
+  actors: [
+    {
+      __typename: 'Actor',
+      id: '1',
+      firstName: 'Harrison',
+      lastName: 'Ford',
+      friends: [
+        {
+          __typename: 'Actor',
+          id: '6',
+          firstName: 'Fred',
+          favHobby: 'sleeping',
+        },
+        {
+          __typename: 'Actor',
+          id: '7',
+          firstName: 'Gary',
+          favHobby: 'climbing',
+        },
+        {
+          __typename: 'Actor',
+          id: '2',
+          firstName: 'Sean',
+          favHobby: 'fishing',
+        },
+      ],
+    },
+    {
+      __typename: 'Actor',
+      id: '2',
+      firstName: 'Sean',
+      lastName: 'Connery',
+      friends: [],
+    },
+  ],
+};
+
+const hashOutput = {
+  'Movie~1': {
+    title: 'Indiana Jones and the Last Crusade',
+    genre: 'ACTION',
+    actors: ['Actor~1', 'Actor~2'],
+  },
+  'Actor~1': {
+    id: '1',
+    firstName: 'Harrison',
+    lastName: 'Ford',
+    friends: ['Actor~6', 'Actor~7', 'Actor~2'],
+  },
+  'Actor~6': {
+    id: '6',
+    firstName: 'Fred',
+    favHobby: 'sleeping',
+  },
+  'Actor~7': {
+    id: '7',
+    firstName: 'Gary',
+    favHobby: 'climbing',
+  },
+  'Actor~2': {
+    id: '2',
+    firstName: 'Sean',
+    lastName: 'Connery',
+    favHobby: 'fishing',
+    friends: [],
+  },
+};
+
+console.log(createHash(hashInput));
 
 // //creates hash:obj pair for complex fields
 // function innerQuery(innerArray) {
