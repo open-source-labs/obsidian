455bfd6f8522934af026456c37c5702545552821
Esma-Sahraoui <esma.sahraoui@gmail.com>
2020-12-30 14:29:37 -0500

    added cache related changes

--- src/mutate.js
+++ src/mutate.js
@@ -2,7 +2,7 @@
  * NOTES:
  * 1. This implementation does not handle variables currently
  * 2. This function will always send the inputted mutation operation string to the inputted endpoint
- * 3. This function would invoke gc() every time a mutation is made except when an update function is provided by the developer.
+ * 3. After implementing garbage collection: This function would invoke gc() every time a mutation is made except when an update function is provided by the developer.
  * 4. This implementation would update the cache only if the flag cache is set to true.
  * 5. This function takes in a mutation string and an optional options object and returns the response object from the request made.
  */
@@ -28,17 +28,17 @@ function mutate(mutation, options = {}) {
     if (!cache) return responseObj;
     // first behaviour when delete cache is set to true
     if (toDelete) {
-      this.write(mutation, responseObj, true);
-      this.gc();
+      cache.write(mutation, responseObj, true);
+      // here we can use cache.gc();
       return responseObj;
     }
     // second behaviour if update function provided
     if (update) {
-      return update(this, responseObj);
+      return update(cache, responseObj);
     }
     // third behaviour just for normal update (no-delete, no update function)
-    this.write(mutation, responseObj);
-    this.gc();
+    cache.write(mutation, responseObj);
+    // here we can use cache.gc();
     return responseObj;
   } catch (e) {
     console.log(e);
