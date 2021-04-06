8b0985ad43e9cd7ccb3e73a14ac674b69d0cf292
Damon Alfaro <djdalfaro@protonmail.com>
2021-04-05 21:39:50 -0500

    lfu testing work in progress

---
+++ test_files/rhum_test_files/lfuBrowserCache_test.ts
@@ -0,0 +1,54 @@
+/**
+ * NOTES:
+ * 1.This file will test the write method on the Cache class functionality:
+ * Should return the new updated cache when the cache was successfully updated with the same reference to the original cache
+// Should return the string 'Cache update' when the cache was successfully updated.
+ * Should update the original cache with the new fields and queries.
+ * Should not overwrite the fields in the original cache with the new fields if the fields are not the same
+ * Should test capacity 
+ */
+
+import LFUCache from '../../src/lfuBrowserCache.js';
+import { Rhum } from 'https://deno.land/x/rhum@v1.1.4/mod.ts';
+import { test } from '../test_variables/lfuBrowserCache_variables.ts';
+
+Rhum.testPlan('LFU Browser Cache Testing', () => {
+  Rhum.testSuite('write/read nested data object', () => {
+    for (let key of test.nestedObj.keys) {
+      Rhum.testCase(
+        'should store a nested data object into LFUCache and read the stored un-nested data objects by calling their hashes',
+        async () => {
+          const cache = new LFUCache(10);
+          await cache.write(test.nestedObj.queryStr, test.nestedObj.respObj);
+          Rhum.asserts.assertEquals(
+            cache.get(key),
+            test.nestedObj.expectedCache[key]
+          );
+        }
+      );
+    }
+  });
+  Rhum.testSuite('LFU cache evict the proper items', () => {
+    Rhum.testCase(
+      'should remove the least frequently used item from cache',
+      async () => {
+        const cache = new LFUCache(5);
+        await cache.write(test.LFUObj.queryStr1, test.LFUObj.respObj1);
+        // Rhum.asserts.assertEquals(
+        //   cache.get('Actor~1'),
+        //   test.expectedCache['Actor~1']
+        // );
+        await console.log(cache);
+        await cache.get('Actor~2');
+        await cache.get('Actor~3');
+        await cache.get('Actor~4');
+        await cache.get('Actor~5');
+        await cache.write(test.LFUObj.queryStr2, test.LFUObj.respObj2);
+        // Rhum.asserts.assertEquals(cache.get('Actor~1'), undefined);
+      }
+    );
+  });
+});
+
+Rhum.run();
+// TO RUN TEST: deno test test_files/rhum_test_files/lfuBrowserCache_test.ts --allow-env
