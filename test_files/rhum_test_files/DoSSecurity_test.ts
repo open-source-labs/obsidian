170e1a8121014f65a6a6499f33dbebe381566c24
Patrick Sullivan <patrick@jsullivan.org>
2021-06-24 15:19:02 -0700

    Added tests for multiple queries or mutations for DoSSecurity module
    Co-authored-by: justinwmckay <justinmckay99@gmail.com>
    Co-authored-by: kyunglee1 <kyunglee3@yahoo.com>
    Co-authored-by: raymondcodes <ray.ahn@gmail.com>
    Co-authored-by: cssim22 <cssim22@gmail.com>
    Co-authored-by: pjmsullivan <patrick@jsullivan.org>

--- test_files/rhum_test_files/DoSSecurity_test.ts
+++ test_files/rhum_test_files/DoSSecurity_test.ts
@@ -5,18 +5,18 @@ import { test } from '../test_variables/DoSSecurity_variables.ts';
 
 Rhum.testPlan('DoSSecurity.ts', () => {
   Rhum.testSuite('Query depth limit NOT exceeded tests', () => {
-    Rhum.testCase('Test query depth with allowable depth 2', () => {
+    Rhum.testCase('Test query depth of 2 does not exceed allowable depth 2', () => {
       const results = queryDepthLimiter(test.DEPTH_2_QUERY, 2);
       Rhum.asserts.assertEquals(undefined, results);
     });
-    Rhum.testCase('Test mutation with allowable depth of 2', () => {
+    Rhum.testCase('Test mutation depth of 2 does not exceed allowable depth of 2', () => {
       const results = queryDepthLimiter(test.DEPTH_2_MUTATION, 2);
       Rhum.asserts.assertEquals(undefined, results);
     });
   });
   
   Rhum.testSuite('Query/mutation depth limit IS EXCEEDED tests', () => {
-    Rhum.testCase('Test query depth should be exceeded', () => {
+    Rhum.testCase('Test query depth 2 should exceed depth limit of 1', () => {
       Rhum.asserts.assertThrows(
         () => {
           queryDepthLimiter(test.DEPTH_2_QUERY, 1)
@@ -25,10 +25,42 @@ Rhum.testPlan('DoSSecurity.ts', () => {
         "Security Error: Query depth exceeded maximum query depth limit",
       )
     });
-    Rhum.testCase('Test mutation depth should be exceeded', () => {
+    Rhum.testCase('Test mutation depth 2 should exceed depth limit of 1', () => {
       Rhum.asserts.assertThrows(
         () => {
           queryDepthLimiter(test.DEPTH_2_MUTATION, 1)
+        },
+        Error,
+        "Security Error: Query depth exceeded maximum mutation depth limit",
+      )
+    });
+  });
+
+  Rhum.testSuite('Query depth limit NOT exceeded, multiple query tests', () => {
+    Rhum.testCase('Test multiple queries of depth 2 should not exceed allowable depth 2', () => {
+      const results = queryDepthLimiter(test.MULTIPLE_DEPTH_2_QUERY, 2);
+      Rhum.asserts.assertEquals(undefined, results);
+    });
+    Rhum.testCase('Test multiple mutations of depth 2 should not exceed allowable depth 2', () => {
+      const results = queryDepthLimiter(test.MULTIPLE_DEPTH_2_MUTATION, 2);
+      Rhum.asserts.assertEquals(undefined, results);
+    });
+  });
+
+  Rhum.testSuite('Multiple query/mutation depth limit IS EXCEEDED tests', () => {
+    Rhum.testCase('Test multiple query depth should be exceeded', () => {
+      Rhum.asserts.assertThrows(
+        () => {
+          queryDepthLimiter(test.MULTIPLE_DEPTH_2_QUERY, 1)
+        },
+        Error,
+        "Security Error: Query depth exceeded maximum query depth limit",
+      )
+    });
+    Rhum.testCase('Test multiple mutation depth should be exceeded', () => {
+      Rhum.asserts.assertThrows(
+        () => {
+          queryDepthLimiter(test.MULTIPLE_DEPTH_2_MUTATION, 1)
         },
         Error,
         "Security Error: Query depth exceeded maximum mutation depth limit",
--- test_files/test_variables/DoSSecurity_variables.ts
+++ test_files/test_variables/DoSSecurity_variables.ts
@@ -27,4 +27,68 @@ export const test = {
       }
     }
   }`,
+
+  MULTIPLE_DEPTH_2_QUERY: `
+  query AllActionMovies {
+    movies(input: { genre: ACTION }) {
+      __typename
+      id
+      title
+      genre
+    },
+    movies(input: { genre: ACTION }) {
+      __typename
+      id
+      title
+      genre
+      actors {
+        id
+        firstName
+        lastName
+      }
+    },
+    movies(input: { genre: ACTION }) {
+      __typename
+      id
+      title
+      genre
+      actors {
+        id
+        firstName
+        lastName
+      }
+    }
+  }`,
+
+  MULTIPLE_DEPTH_2_MUTATION: `
+  mutation AllActionMovies {
+    movies(input: { genre: ACTION }) {
+      __typename
+      id
+      title
+      genre
+    },
+    movies(input: { genre: ACTION }) {
+      __typename
+      id
+      title
+      genre
+      actors {
+        id
+        firstName
+        lastName
+      }
+    },
+    movies(input: { genre: ACTION }) {
+      __typename
+      id
+      title
+      genre
+      actors {
+        id
+        firstName
+        lastName
+      }
+    }
+  }`,
 };
