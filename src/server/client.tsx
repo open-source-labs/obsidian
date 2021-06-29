906ee65fd0bae8cc418bedf8b068f6eadb321f48
Kyung Lee <46550288+kyunglee1@users.noreply.github.com>
2021-06-29 11:31:18 -0700

    Remove  unnecessary files
        Co-authored-by: justinwmckay <justinmckay99@gmail.com>
        Co-authored-by: raymondcodes <ray.ahn@gmail.com>
        Co-authored-by: cssim22 <cssim22@gmail.com>
        Co-authored-by: pjmsullivan <patrick@jsullivan.org>

--- src/server/client.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'https://dev.jspm.io/react';
-import ReactDom from 'https://dev.jspm.io/react-dom';
-import App from './App.tsx';
-
-(ReactDom as any).hydrate(<App />, document.getElementById('root'));
