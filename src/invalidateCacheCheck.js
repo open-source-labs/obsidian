1120c932f25b4ade79d1ff773e4a2615915b3af6
cjamesb <cjberry1234@sbcglobal.net>
2021-08-16 13:55:25 -0700

    Cleaned up files that were not doing anything

--- src/invalidateCacheCheck.js
+++ src/invalidateCacheCheck.js
@@ -4,29 +4,24 @@ import { print, visit } from "https://deno.land/x/graphql_deno/mod.ts";
 import { redisdb } from "./quickCache.js";
 
 export function invalidateCacheCheck(body) {
-  // console.log("Inside invalidateCachecheck, body:", body);
-  let isSubscription=false;
+  let isSubscription = false;
   let ast = gql(body.query);
-
-  //console.log("Inside invalidateCachecheck", ast);
 
   const checkMutationVisitor = {
     OperationDefinition: (node) => {
       if (node.operation === "mutation") {
         //console.log("now we nuke the redis");
         redisdb.flushdb();
-      };
+      }
     },
   };
   const subscriptionTunnelVisitor = {
     OperationDefinition: (node) => {
       if (node.operation === "subscription") {
-        isSubscription = true
-
+        isSubscription = true;
       }
-    }
-
-  }
+    },
+  };
   visit(ast, { enter: subscriptionTunnelVisitor, leave: checkMutationVisitor });
   return isSubscription;
 }
