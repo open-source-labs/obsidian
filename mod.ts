2a254f3e08e60cc3b8ff8e1ad5f661022343e033
cjamesb <cjberry1234@sbcglobal.net>
2021-08-17 15:14:36 -0700

    renamed newObs to Obsidian

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { ObsidianRouter } from './src/newObsidian.ts';
+import { ObsidianRouter } from './src/Obsidian.ts';
 import gql from 'https://deno.land/x/oak_graphql@0.6.2/graphql-tag/index.ts';
 
 export { ObsidianRouter, gql };
