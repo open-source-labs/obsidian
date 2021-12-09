f1f339e98223f464555e7bf050df2f8a3881acdd
Sardor Akhmedov <sarkamedo@gmail.com>
2021-12-09 16:43:13 -0500

    Added features for invalidation, refactored normalization logic, refactored read and write to cache
    Co-authored by: Yogi Paturu <ypaturu@gmail.com>
    Co-authored by: Michael Chin <mikechin37@gmail.com>
    Co-authored by: Sardor Akhmedov <sarkamedo@gmail.com>
    Co-authored by: Dana Flury <dmflury@gmail.com>

--- src/invalidateCacheCheck.js
+++ src/invalidateCacheCheck.js
@@ -3,15 +3,13 @@ import { gql } from "https://deno.land/x/oak_graphql/mod.ts";
 import { visit } from "https://deno.land/x/graphql_deno/mod.ts";
 import { Cache } from "./quickCache.js";
 import { deepEqual } from "./utils.js";
-import {containsHashableObject, isHashableObject, hashMaker, printHashableObject, normalizeObject} from './normalize.ts'
-
 
 const cache = new Cache();
 
 export function invalidateCacheCheck(body) {
   let isMutation = false;
   let ast = gql(body.query);
-
+  
   const checkMutationVisitor = {
     OperationDefinition: (node) => {
       if (node.operation === "mutation") {
@@ -35,24 +33,42 @@ export function invalidateCacheCheck(body) {
 export async function invalidateCache(normalizedMutation) {
 
   for (const redisKey in normalizedMutation) {
-    const normalizedData = normalizedMutation[key];
+    const normalizedData = normalizedMutation[redisKey];
     const cachedVal = await cache.cacheReadObject(redisKey);
 
-    // if no value in redis at @redisKey then it's an add mution so we write the added value to cache
-    if (cachedVal === undefined) {
-      await cache.cacheWriteObject(redisKey, normalizedData)
-      return 'Add mutation -> Cache write performed'
-    }
-
     // if response from mutation and cached response values are same then it's a delete mutation
-    if (deepEqual(normalizedData, cachedVal)) {
+    // edge case not covered for delete mutation: if value is not currently cached in redis it will be treated as add mutation and stale value will be written to redis 
+    if (cachedVal !== undefined && deepEqual(normalizedData, cachedVal)) {
       await cache.cacheDelete(redisKey)
       return 'Delete mutation -> Cache delete performed'
     } else {
       // otherwise it's an update mutation because mutation response doesn't match cached response values so we overwrite it
       // Edge case: update is done without changing any values... cache will be deleted from redis and needs to be rewritten to cache on the next request.
       await cache.cacheWriteObject(redisKey, normalizedData)
-      return 'Update mutation -> Cache overwrite performed'
+      return 'Update or Add mutation -> Cache overwrite performed'
     }
   }
 }
+
+
+
+// THE BELOW NEEDS WORK TO BREAK UP THE CACHE INVALIDATION FUNCTIONALITY
+
+// const isAddMutation = (cachedVal) => {
+//   (cachedVal === undefined) ? true : false;
+// }
+
+// const isDeleteMutation = (cachedVal, normalizedResponseObjVal) => {
+//     deepEqual(normalizedData, cachedVal) ? true : false
+// }
+
+// const isUpdateMutation = (cachedVal, normalizedResponseObjVal) => {
+//     if(!isAddMutation(cachedVal) && !isDeleteMutation(cachedVal, normalizedResponseObjVal)) return true;
+//     return false;
+// }
+
+// const cacheInvalidationJS = async (redisKey, cachedVal, normalizedResponseObjVal) => {
+//   if(isAddMutation(cachedVal)) await cache.cacheWriteObject(redisKey, normalizedResponseObjVal)
+//   else if(isDeleteMutation(cachedVal, normalizedResponseObjVal)) await cache.cacheDelete(redisKey)
+//   else if(isUpdateMutation(cachedVal, normalizedResponseObjVal)) await cache.cacheWriteObject(redisKey, normalizedResponseObjVal)
+// }
