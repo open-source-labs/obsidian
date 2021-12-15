25b9e3b33231a4897ad341ec33c3373cc5d5400a
Sardor Akhmedov <sarkamedo@gmail.com>
2021-12-15 12:36:30 -0500

    completed normalizeObject tests

--- test_files/rhum_test_files/serverNormalize_test.js
+++ test_files/rhum_test_files/serverNormalize_test.js
@@ -1,60 +1,70 @@
-import { assert, equal, assertStrictEquals, assertEquals} from "https://deno.land/std/testing/asserts.ts";
-import { containsHashableObject, isHashableObject, hashMaker, printHashableObject, normalizeObject} from '../../src/normalize.ts';
-import { serverNormalizeTestVariables } from '../test_variables/serverNormalize_variables.ts';
+import { assert, equal, assertStrictEquals, assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { containsHashableObject, isHashableObject, hashMaker, printHashableObject, normalizeObject } from '../../src/normalize.ts';
+import { serverNormalizeTestVariables as data } from '../test_variables/serverNormalize_variables.ts';
 const arrOfHashableKeys = ['id', '__typename'];
 
 // containsHashableObject
 Deno.test('normalize.ts - cointainsHashableObject - True test 1: object with hashable key properties and a nested object', () => {
-    assert(containsHashableObject(serverNormalizeTestVariables.containsHashableObjTrue1, arrOfHashableKeys) === true);
+  assert(containsHashableObject(data.containsHashableObjTrue1, arrOfHashableKeys) === true);
 })
 Deno.test('normalize.ts - cointainsHashableObject - True test 2: object with hashable key properties and an array of nested objects', () => {
-    assert(containsHashableObject(serverNormalizeTestVariables.containsHashableObjTrue2, arrOfHashableKeys) === true);
+  assert(containsHashableObject(data.containsHashableObjTrue2, arrOfHashableKeys) === true);
 })
 Deno.test('normalize.ts - cointainsHashableObject - True test 3: object with hashable key properties with nested array', () => {
-    assert(containsHashableObject(serverNormalizeTestVariables.containsHashableObjTrue3, arrOfHashableKeys) === true);
+  assert(containsHashableObject(data.containsHashableObjTrue3, arrOfHashableKeys) === true);
 })
 Deno.test('normalize.ts - cointainsHashableObject - False test 1: array of hashable keys', () => {
-    assert(containsHashableObject(serverNormalizeTestVariables.containsHashableObjFalse1, arrOfHashableKeys) === false);
+  assert(containsHashableObject(data.containsHashableObjFalse1, arrOfHashableKeys) === false);
 })
 Deno.test('normalize.ts - cointainsHashableObject - False test 2: array nested with an object of hashable key properties', () => {
-    assert(containsHashableObject(serverNormalizeTestVariables.containsHashableObjFalse2, arrOfHashableKeys) === false);
+  assert(containsHashableObject(data.containsHashableObjFalse2, arrOfHashableKeys) === false);
 })
 Deno.test('normalize.ts - cointainsHashableObject - False test 3: object with nested object with hashable key properties', () => {
-    assert(containsHashableObject(serverNormalizeTestVariables.containsHashableObjFalse3, arrOfHashableKeys) === false);
+  assert(containsHashableObject(data.containsHashableObjFalse3, arrOfHashableKeys) === false);
 })
 
 // isHashableObject
 Deno.test('normalize.ts - isHashableObject - True test 1: object with hashable key properties and no nesting', () => {
-    assert(isHashableObject(serverNormalizeTestVariables.isHashableObjTrue1, arrOfHashableKeys) === true);
+  assert(isHashableObject(data.isHashableObjTrue1, arrOfHashableKeys) === true);
 })
 Deno.test('normalize.ts - isHashableObject - True test 2: object with hashable key properties and no nesting', () => {
-    assert(isHashableObject(serverNormalizeTestVariables.isHashableObjTrue2, arrOfHashableKeys) === true);
+  assert(isHashableObject(data.isHashableObjTrue2, arrOfHashableKeys) === true);
 })
 Deno.test('normalize.ts - isHashableObject - False test 1: array of hashable keys', () => {
-    assert(isHashableObject(serverNormalizeTestVariables.isHashableObjFalse1, arrOfHashableKeys) === false);
+  assert(isHashableObject(data.isHashableObjFalse1, arrOfHashableKeys) === false);
 })
 Deno.test('normalize.ts - isHashableObject - False test 2: object with hashable key properties and nesting', () => {
-    assert(isHashableObject(serverNormalizeTestVariables.isHashableObjFalse2, arrOfHashableKeys) === false);
+  assert(isHashableObject(data.isHashableObjFalse2, arrOfHashableKeys) === false);
 })
 
 // hashMaker
 Deno.test('normalize.ts - hashMaker - Creates unique hash when a hashable object is passed through', () => {
-    console.log(hashMaker(serverNormalizeTestVariables.isHashableObjTrue1, arrOfHashableKeys))
-    assertStrictEquals(hashMaker(serverNormalizeTestVariables.isHashableObjTrue1, arrOfHashableKeys), "~7~Movie");
+  assertStrictEquals(hashMaker(data.isHashableObjTrue1, arrOfHashableKeys), "~7~Movie");
 })
 Deno.test('normalize.ts - hashMaker - Creates unique hash when a hashable object is passed through', () => {
-    assertStrictEquals(hashMaker(serverNormalizeTestVariables.isHashableObjTrue2, arrOfHashableKeys), "~1~Actor");
+  assertStrictEquals(hashMaker(data.isHashableObjTrue2, arrOfHashableKeys), "~1~Actor");
 })
 
 // printHashableObject
 Deno.test('normalize.ts - printHashableObject - Prints a hashable object when object with hashable key properties and no nesting is passed through', () => {
-    assertEquals(printHashableObject(serverNormalizeTestVariables.containsHashableObjTrue1), {"id": "11", "__typename": "Movie", "title": "Ad Astra"});
+  assertEquals(printHashableObject(data.containsHashableObjTrue1), { "id": "11", "__typename": "Movie", "title": "Ad Astra" });
 })
 Deno.test('normalize.ts - printHashableObject - Prints a hashable object when object with hashable key properties and no nesting is passed through', () => {
-    assertEquals(printHashableObject(serverNormalizeTestVariables.containsHashableObjTrue2), {"id": "7", "__typename": "Movie", "title": "Ad Astra", "releaseYear": 2019, "genre": "SCIFI"});
+  assertEquals(printHashableObject(data.containsHashableObjTrue2), { "id": "7", "__typename": "Movie", "title": "Ad Astra", "releaseYear": 2019, "genre": "SCIFI" });
 })
 Deno.test('normalize.ts - printHashableObject - Prints a hashable object when object with hashable key properties and no nesting is passed through', () => {
-    assertEquals(printHashableObject(serverNormalizeTestVariables.containsHashableObjTrue3), {"id": "1", "__typename": "Actor", "firstName": "Brad",  "lastName": "Pitt"});
+  assertEquals(printHashableObject(data.containsHashableObjTrue3), { "id": "1", "__typename": "Actor", "firstName": "Brad", "lastName": "Pitt" });
 })
 
 // normalizeObject
+Deno.test('normalize.ts - normalizeObject - Constructs an object of reference caches. Key being the hash used as redis key and value being object that is stored in redis', () => {
+  assertEquals(normalizeObject(data.scifiMovies, ["id", "__typename"]), data.scifiMoviesNormalized);
+})
+
+Deno.test('normalize.ts - normalizeObject - Constructs an object of reference caches. Key being the hash used as redis key and value being object that is stored in redis', () => {
+  assertEquals(normalizeObject(data.arbitraryNestedScifiMovies, ["id", "__typename"]), data.arbitraryNestedScifiMoviesNormalized);
+})
+
+Deno.test('normalize.ts - normalizeObject - returns empty object if values in customIdentifier argument are not found in the nested response object', () => {
+  assertEquals(normalizeObject(data.scifiMovies, ['identifier', 'uid', 'someType']), {});
+})
--- test_files/test_variables/serverNormalize_variables.ts
+++ test_files/test_variables/serverNormalize_variables.ts
@@ -154,6 +154,28 @@ serverNormalizeTestVariables.scifiMovies = {
         ]
     }
 }
+serverNormalizeTestVariables.scifiMoviesNormalized = {
+  "~7~Movie": { id: "7", __typename: "Movie", title: "Ad Astra", releaseYear: 2019, genre: "SCIFI" },
+  "~1~Actor": { id: "1", __typename: "Actor", firstName: "Brad", lastName: "Pitt" },
+  "~14~Actor": { id: "14", __typename: "Actor", firstName: "Tommy Lee", lastName: "Jones" },
+  "~15~Movie": {
+    id: "15",
+    __typename: "Movie",
+    title: "World War Z",
+    releaseYear: 2013,
+    genre: "SCIFI"
+  },
+  "~17~Movie": {
+    id: "17",
+    __typename: "Movie",
+    title: "Sky Captain and the World of Tomorrow",
+    releaseYear: 2004,
+    genre: "SCIFI"
+  },
+  "~2~Actor": { id: "2", __typename: "Actor", firstName: "Angelina", lastName: "Jolie" },
+  "~25~Actor": { id: "25", __typename: "Actor", firstName: "Jude", lastName: "Law" },
+  "~26~Actor": { id: "26", __typename: "Actor", firstName: "Gwyneth", lastName: "Paltrow" }
+}
 serverNormalizeTestVariables.arbitraryNestedScifiMovies =
 {
     "data": {
@@ -15316,5 +15338,170 @@ serverNormalizeTestVariables.arbitraryNestedScifiMovies =
       ]
     }
 }
-
-// console.log(printHashableObject(serverNormalizeTestVariables.containsHashableObjTrue1))
+serverNormalizeTestVariables.arbitraryNestedScifiMoviesNormalized = {
+  "~7~Movie": { id: "7", __typename: "Movie", title: "Ad Astra", releaseYear: 2019, genre: "SCIFI" },
+  "~1~Actor": { id: "1", __typename: "Actor", firstName: "Brad", lastName: "Pitt" },
+  "~1~Movie": {
+    id: "1",
+    __typename: "Movie",
+    title: "Mr. and Mrs. Smith",
+    releaseYear: 2005,
+    genre: "COMEDY"
+  },
+  "~2~Actor": { id: "2", __typename: "Actor", firstName: "Angelina", lastName: "Jolie" },
+  "~2~Movie": {
+    id: "2",
+    __typename: "Movie",
+    title: "Fight Club",
+    releaseYear: 1999,
+    genre: "ACTION"
+  },
+  "~4~Actor": { id: "4", __typename: "Actor", firstName: "Edward", lastName: "Norton" },
+  "~5~Actor": { id: "5", __typename: "Actor", firstName: "Helena", lastName: "Bonharm Carter" },
+  "~3~Movie": { id: "3", __typename: "Movie", title: "Troy", releaseYear: 2004, genre: "ADVENTURE" },
+  "~6~Actor": { id: "6", __typename: "Actor", firstName: "Orlando", lastName: "Bloom" },
+  "~4~Movie": {
+    id: "4",
+    __typename: "Movie",
+    title: "Oceans 11",
+    releaseYear: 2001,
+    genre: "ACTION"
+  },
+  "~7~Actor": { id: "7", __typename: "Actor", firstName: "George", lastName: "Clooney" },
+  "~8~Actor": { id: "8", __typename: "Actor", firstName: "Matt", lastName: "Damon" },
+  "~9~Actor": { id: "9", __typename: "Actor", firstName: "Julia", lastName: "Roberts" },
+  "~5~Movie": {
+    id: "5",
+    __typename: "Movie",
+    title: "Once Upon a Time in Hollywood",
+    releaseYear: 2019,
+    genre: "DRAMA"
+  },
+  "~3~Actor": { id: "3", __typename: "Actor", firstName: "Leonardo", lastName: "Dicaprio" },
+  "~10~Actor": { id: "10", __typename: "Actor", firstName: "Margot", lastName: "Robbie" },
+  "~6~Movie": {
+    id: "6",
+    __typename: "Movie",
+    title: "Moneyball",
+    releaseYear: 2011,
+    genre: "DRAMA"
+  },
+  "~11~Actor": { id: "11", __typename: "Actor", firstName: "Jonah", lastName: "Hill" },
+  "~12~Actor": { id: "12", __typename: "Actor", firstName: "Philip", lastName: "Seymour Hoffman" },
+  "~13~Actor": { id: "13", __typename: "Actor", firstName: "Chris", lastName: "Pratt" },
+  "~14~Actor": { id: "14", __typename: "Actor", firstName: "Tommy Lee", lastName: "Jones" },
+  "~15~Movie": {
+    id: "15",
+    __typename: "Movie",
+    title: "World War Z",
+    releaseYear: 2013,
+    genre: "SCIFI"
+  },
+  "~16~Movie": {
+    id: "16",
+    __typename: "Movie",
+    title: "Legends of the Fall",
+    releaseYear: 1994,
+    genre: "ROMANCE"
+  },
+  "~24~Actor": { id: "24", __typename: "Actor", firstName: "Anthony", lastName: "Hopkins" },
+  "~20~Movie": {
+    id: "20",
+    __typename: "Movie",
+    title: "By the Sea",
+    releaseYear: 2015,
+    genre: "ROMANCE"
+  },
+  "~8~Movie": {
+    id: "8",
+    __typename: "Movie",
+    title: "Maleficient",
+    releaseYear: 2014,
+    genre: "ADVENTURE"
+  },
+  "~9~Movie": {
+    id: "9",
+    __typename: "Movie",
+    title: "Lara Croft Tomb Raider",
+    releaseYear: 2001,
+    genre: "ADVENTURE"
+  },
+  "~15~Actor": { id: "15", __typename: "Actor", firstName: "Daniel", lastName: "Craig" },
+  "~10~Movie": { id: "10", __typename: "Movie", title: "Wanted", releaseYear: 2008, genre: "ACTION" },
+  "~16~Actor": { id: "16", __typename: "Actor", firstName: "James", lastName: "McAvoy" },
+  "~17~Actor": { id: "17", __typename: "Actor", firstName: "Morgan", lastName: "Freeman" },
+  "~11~Movie": { id: "11", __typename: "Movie", title: "Salt", releaseYear: 2010, genre: "ACTION" },
+  "~12~Movie": {
+    id: "12",
+    __typename: "Movie",
+    title: "Gone in 60 Seconds",
+    releaseYear: 2000,
+    genre: "ACTION"
+  },
+  "~18~Actor": { id: "18", __typename: "Actor", firstName: "Nicholas", lastName: "Cage" },
+  "~13~Movie": {
+    id: "13",
+    __typename: "Movie",
+    title: "Kung Fu Panda",
+    releaseYear: 2008,
+    genre: "COMEDY"
+  },
+  "~19~Actor": { id: "19", __typename: "Actor", firstName: "Jack", lastName: "Black" },
+  "~20~Actor": { id: "20", __typename: "Actor", firstName: "Dustin", lastName: "Hoffman" },
+  "~21~Actor": { id: "21", __typename: "Actor", firstName: "Jackie", lastName: "Chan" },
+  "~22~Actor": { id: "22", __typename: "Actor", firstName: "Seth", lastName: "Rogen" },
+  "~14~Movie": {
+    id: "14",
+    __typename: "Movie",
+    title: "The Tourist",
+    releaseYear: 2010,
+    genre: "ROMANCE"
+  },
+  "~23~Actor": { id: "23", __typename: "Actor", firstName: "Johnny", lastName: "Depp" },
+  "~17~Movie": {
+    id: "17",
+    __typename: "Movie",
+    title: "Sky Captain and the World of Tomorrow",
+    releaseYear: 2004,
+    genre: "SCIFI"
+  },
+  "~25~Actor": { id: "25", __typename: "Actor", firstName: "Jude", lastName: "Law" },
+  "~26~Actor": { id: "26", __typename: "Actor", firstName: "Gwyneth", lastName: "Paltrow" },
+  "~18~Movie": {
+    id: "18",
+    __typename: "Movie",
+    title: "Girl, Interrupted",
+    releaseYear: 1999,
+    genre: "DRAMA"
+  },
+  "~27~Actor": { id: "27", __typename: "Actor", firstName: "Winona", lastName: "Ryder" },
+  "~19~Movie": {
+    id: "19",
+    __typename: "Movie",
+    title: "Playing By Heart",
+    releaseYear: 1998,
+    genre: "ROMANCE"
+  },
+  "~28~Actor": { id: "28", __typename: "Actor", firstName: "Sean", lastName: "Connery" },
+  "~21~Movie": {
+    id: "21",
+    __typename: "Movie",
+    title: "Beowulf",
+    releaseYear: 2007,
+    genre: "ADVENTURE"
+  },
+  "~22~Movie": {
+    id: "22",
+    __typename: "Movie",
+    title: "The Good Shepherd",
+    releaseYear: 2006,
+    genre: "DRAMA"
+  },
+  "~23~Movie": {
+    id: "23",
+    __typename: "Movie",
+    title: "Shark Tale",
+    releaseYear: 2004,
+    genre: "COMEDY"
+  }
+}
