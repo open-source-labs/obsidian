5e21f8e3ef86408e9a7ee1218bbbdd758e4ce642
Josh Reed <joshreed104@gmail.com>
2022-12-23 11:14:36 -0800

    Implemented selective querying from cache and cache invalidation on mutations to a given table.
    Co-authored-by: Jonathan Fangon <jmfangon@icloud.com>
    Co-authored-by: Liam Johnson <liamd@live.com>
    Co-authored-by: Derek Okuno <okunoderek@gmail.com>
    Co-authored-by: Liam Jeon <liamjeon@gmail.com>
    Co-authored-by: Josh Reed <joshreed104@gmail.com>

--- src/normalize.ts
+++ src/normalize.ts
@@ -10,18 +10,23 @@
  * @return {boolean} Boolean indicating if objectInQuestion is hashable or not
  */
 
- export const containsHashableObject = (objectInQuestion: any, hashableKeys: Array<string>):boolean => {
-    if(typeof objectInQuestion !== 'object' ||
-        Array.isArray(objectInQuestion) ||
-        !objectInQuestion
-    ) return false;
-    const objectInQuestionKeysSet = new Set(Object.keys(objectInQuestion));
-    return hashableKeys.every(key => objectInQuestionKeysSet.has(key))
-}
+export const containsHashableObject = (
+  objectInQuestion: any,
+  hashableKeys: Array<string>
+): boolean => {
+  if (
+    typeof objectInQuestion !== 'object' ||
+    Array.isArray(objectInQuestion) ||
+    !objectInQuestion
+  )
+    return false;
+  const objectInQuestionKeysSet = new Set(Object.keys(objectInQuestion));
+  return hashableKeys.every((key) => objectInQuestionKeysSet.has(key));
+};
 /* ----------------------------------------------------------------*/
 
 /* ----------------------------------------------------------------*/
-/** isHashableObject - 
+/** isHashableObject -
  * Returns a boolean indicating that the passed in value is hashable. It must:
  * 1) Contain hashable object
  * 2) Does not have any nesting (i.e., contains no objects or array values)
@@ -30,85 +35,109 @@
  * @param {Array<string>} hashableKeys Array of hashable keys
  * @return {boolean} Boolean indicating if objectInQuestion is hashable or not
  */
-export const isHashableObject = (objectInQuestion: any, hashableKeys: Array<string>): boolean => {
+export const isHashableObject = (
+  objectInQuestion: any,
+  hashableKeys: Array<string>
+): boolean => {
   if (!containsHashableObject(objectInQuestion, hashableKeys)) return false;
   for (const key in objectInQuestion) {
     if (typeof objectInQuestion[key] === 'object') return false;
   }
   return true;
-}
+};
 /* ----------------------------------------------------------------*/
 
-
 /* ----------------------------------------------------------------*/
-export type GenericObject = { [key:string]: any};
-type FlatObject = { [key:string]: (string | number | boolean)};
+export type GenericObject = { [key: string]: any };
+type FlatObject = { [key: string]: string | number | boolean };
 /** hashMaker -
  * Creates unique hash string for an object with hashable keys with hashable object passed in
  *
- * @param {FlatObject} hashableObject Object that is hashable 
+ * @param {FlatObject} hashableObject Object that is hashable
  * @param {Array<string>} hashableKeys Array of hashable keys
  * @return {string} Hash string
  */
-export const hashMaker = (hashableObject: FlatObject, hashableKeys:Array<string>):string => {   
-    let hash = '';
-    let value = '';
-    for(const hashableKey of hashableKeys){
-        value = '~';
-        value += hashableObject[hashableKey]
-        hash += value;
-    }
-    return hash;
-}
+export const hashMaker = (
+  hashableObject: FlatObject,
+  hashableKeys: Array<string>
+): string => {
+  let hash = '';
+  let value = '';
+  for (const hashableKey of hashableKeys) {
+    value = '~';
+    value += hashableObject[hashableKey];
+    hash += value;
+  }
+  return hash;
+};
 /* ----------------------------------------------------------------*/
 
 /* ----------------------------------------------------------------*/
 /** printHashableObject -
- * Creates a hashable object from an object that contains a hashable object. Does not print hashable object 
- * 
- * @param {FlatObject} containsHashableObject Object that is hashable 
+ * Creates a hashable object from an object that contains a hashable object. Does not print hashable object
+ *
+ * @param {FlatObject} containsHashableObject Object that is hashable
  * @return {GenericObject} A hashable object
  */
-export const printHashableObject = (containsHashableObject: GenericObject):GenericObject => {
-    const hashObj:GenericObject = {};
-    for(const key in containsHashableObject){
-        if(typeof containsHashableObject[key] !== 'object' && !hashObj.hasOwnProperty(key)) hashObj[key] = containsHashableObject[key];
-    }
-    return hashObj;
-}
+export const printHashableObject = (
+  containsHashableObject: GenericObject
+): GenericObject => {
+  const hashObj: GenericObject = {};
+  for (const key in containsHashableObject) {
+    if (
+      typeof containsHashableObject[key] !== 'object' &&
+      !hashObj.hasOwnProperty(key)
+    )
+      hashObj[key] = containsHashableObject[key];
+  }
+  return hashObj;
+};
 /* ----------------------------------------------------------------*/
 
 /* ----------------------------------------------------------------*/
 
 /**
  * Recursively flattens an arbitrarily nested object into an objects with hash key and hashable object pairs
- * 
+ *
  * For each key in object (typeof === 'object', meaning it can be array):
- * 
+ *
  * 1) If current object contains hashable object and if it hasn't printed already,
  * it prints a hashable object and makes its associated hash. If hash doesn't exist in normalizedHashableObjects,
  * it adds hash key and hashable object pair.
- * 
+ *
  * 2) If the value at the current key is an object (typeof === 'object', meaning it can be array), it recursively
  * calls normalizeObject with the value passed in. This recursive calls goes inside arbitrary nesting.
- * 
+ *
  * 3) Return normalizedHashableObjects. In the outer most execution context, this will return the output of the function.
  * In inner execution contexts, this will return that execution context's normalizedHashableObjects.
- * 
+ *
  * @param {GenericObject} nestedObject Nested object
- * @param {Array<string>} hashableKeys Array of hashable keys 
+ * @param {Array<string>} hashableKeys Array of hashable keys
  * @return {FlatObject} Normalized object with hash keys and hashable object pairs
  */
-export const normalizeObject = (nestedObject: GenericObject, hashableKeys:Array<string>, normalizedHashableObjects:GenericObject = {}):GenericObject => {
-    let hasAlreadyPrinted = false;
-    for(const key in nestedObject){    
-        if(containsHashableObject(nestedObject, hashableKeys) && hasAlreadyPrinted === false){
-            hasAlreadyPrinted = true;
-            const hashableObject = printHashableObject(nestedObject);
-            const hash = hashMaker(hashableObject, hashableKeys);
-            if(!normalizedHashableObjects.hasOwnProperty(hash)) normalizedHashableObjects[hash] = hashableObject;
-        }
-        if(typeof nestedObject[key] === 'object') normalizeObject(nestedObject[key], hashableKeys, normalizedHashableObjects);
+export const normalizeObject = (
+  nestedObject: GenericObject,
+  hashableKeys: Array<string>,
+  normalizedHashableObjects: GenericObject = {}
+): GenericObject => {
+  let hasAlreadyPrinted = false;
+  for (const key in nestedObject) {
+    if (
+      containsHashableObject(nestedObject, hashableKeys) &&
+      hasAlreadyPrinted === false
+    ) {
+      hasAlreadyPrinted = true;
+      const hashableObject = printHashableObject(nestedObject);
+      const hash = hashMaker(hashableObject, hashableKeys);
+      if (!normalizedHashableObjects.hasOwnProperty(hash))
+        normalizedHashableObjects[hash] = hashableObject;
     }
-    return normalizedHashableObjects;
-}
+    if (typeof nestedObject[key] === 'object')
+      normalizeObject(
+        nestedObject[key],
+        hashableKeys,
+        normalizedHashableObjects
+      );
+  }
+  return normalizedHashableObjects;
+};
--- src/restructure.ts
+++ src/restructure.ts
@@ -1,6 +1,6 @@
 import { gql } from 'https://deno.land/x/oak_graphql/mod.ts';
 
-import {print, visit} from "https://deno.land/x/graphql_deno/mod.ts";
+import { print, visit } from 'https://deno.land/x/graphql_deno/mod.ts';
 
 /**
  * The restructure function:
@@ -11,139 +11,151 @@ import {print, visit} from "https://deno.land/x/graphql_deno/mod.ts";
  * @param  {any} value - Query string
  * @return {string} string
  */
-export function restructure (value:any){
+export function restructure(value: any) {
+  const variables = value.variables || {};
+  const operationName = value.operationName;
 
-   
-    const variables = value.variables || {};
-    const operationName = value.operationName;
-   
-    let ast = gql(value.query);
-    
-    
-    let fragments: {[key:string]:any} = {};
-    let containsFrags:boolean = false;
-    let existingFrags: {[key:string]:any}={};
-    let existingVars: {[key:string]:any}={};
+  let ast = gql(value.query);
+  // console.log('ast from restructure', ast);
+  // console.log(
+  //   ast.definitions[0].selectionSet.selections[0].name,
+  //   ast.definitions[0].selectionSet.selections[0].name.value
+  // );
+  // const name = ast.definitions[0].selectionSet.selections[0].name.value;
+  // const fieldsArray =
+  //   ast.definitions[0].selectionSet.selections[0].arguments[0].value.fields;
+  // const resultsObj = {};
+  // fieldsArray.forEach((el) => {
+  //   const name = el.name.value;
+  //   const value = el.value.value;
+  //   resultsObj[name] = value;
+  // });
+  // console.log(resultsObj);
+  // let cacheHash = `${name}`;
+  // for (let key in resultsObj) {
+  //   cacheHash += `:${key}:${resultsObj[key]}`;
+  // }
+  // console.log('cacheHash: ', cacheHash);
+  // console.log(
+  //   ast.definitions[0].selectionSet.selections[0].arguments[0].value.fields[0],
+  //   ast.definitions[0].selectionSet.selections[0].arguments[0].value.fields[0]
+  //     .name.value,
+  //   ast.definitions[0].selectionSet.selections[0].arguments[0].value.fields[0]
+  //     .value.value
+  // );
 
-    const buildFragsVisitor = {
-      FragmentDefinition:(node:any)=>{
-        fragments[node.name.value]=node.selectionSet.selections;
-       
-      }
-    };  
-    const buildDefaultVarsVisitor = {
-      VariableDefinition:(node:any)=>{
-        
-        if (node.defaultValue){
-          
-          if(!variables[node.variable.name.value]){
-            variables[node.variable.name.value] = node.defaultValue.value;
-          }
-         
+  let fragments: { [key: string]: any } = {};
+  let containsFrags: boolean = false;
+  let existingFrags: { [key: string]: any } = {};
+  let existingVars: { [key: string]: any } = {};
+
+  const buildFragsVisitor = {
+    FragmentDefinition: (node: any) => {
+      fragments[node.name.value] = node.selectionSet.selections;
+    },
+  };
+  const buildDefaultVarsVisitor = {
+    VariableDefinition: (node: any) => {
+      if (node.defaultValue) {
+        if (!variables[node.variable.name.value]) {
+          variables[node.variable.name.value] = node.defaultValue.value;
         }
-  
-    }
+      }
+    },
   };
 
   const rewriteVarsVistor = {
     VariableDefinition: (node: any) => {
       return null;
     },
-    Variable:(node:any)=>{
-      if(variables.hasOwnProperty(node.name.value)){
-        return {kind: "EnumValue", value: variables[node.name.value]};
-        }
+    Variable: (node: any) => {
+      if (variables.hasOwnProperty(node.name.value)) {
+        return { kind: 'EnumValue', value: variables[node.name.value] };
       }
-  
+    },
   };
-  
-  
-    const rewriteVisitor = {
-      FragmentSpread:(node:any)=>{
-        if(fragments.hasOwnProperty(node.name.value)){
-  
-        
-          return fragments[node.name.value];
-        }
-      },
-    };
-  
-    const clearFragVisitor = {
-    FragmentDefinition:(node:any)=>{
-        if(fragments.hasOwnProperty(node.name.value)){
-          return null;
-        }
+
+  const rewriteVisitor = {
+    FragmentSpread: (node: any) => {
+      if (fragments.hasOwnProperty(node.name.value)) {
+        return fragments[node.name.value];
       }
-    }
-    const checkFragmentationVisitor = {
-      FragmentSpread:(node:any)=>{
-       
-        containsFrags = true;
-        existingFrags[node.name.value]=true
-      },
-      Variable:(node:any)=>{
-        containsFrags = true;
-        existingVars[node.name.value]=true
+    },
+  };
+
+  const clearFragVisitor = {
+    FragmentDefinition: (node: any) => {
+      if (fragments.hasOwnProperty(node.name.value)) {
+        return null;
       }
-    }
-  
-    const firstBuildVisitor = {
-      ...buildFragsVisitor,
-      ...buildDefaultVarsVisitor
-    };
-    
-    
-    const firstRewriteVisitor={
+    },
+  };
+  const checkFragmentationVisitor = {
+    FragmentSpread: (node: any) => {
+      containsFrags = true;
+      existingFrags[node.name.value] = true;
+    },
+    Variable: (node: any) => {
+      containsFrags = true;
+      existingVars[node.name.value] = true;
+    },
+  };
+
+  const firstBuildVisitor = {
+    ...buildFragsVisitor,
+    ...buildDefaultVarsVisitor,
+  };
+
+  const firstRewriteVisitor = {
     ...rewriteVisitor,
     ...rewriteVarsVistor,
-    OperationDefinition:(node:any)=>{
-      if(operationName&&node.name.value!=operationName){return null}},
-      InlineFragment:(node:any)=>{
-              return [{
-                kind: "Field",
-                alias: undefined,
-                name: { kind: "Name", value: "__typename" },
-                arguments: [],
-                directives: [],
-                selectionSet: undefined
-              },node]
-
-
+    OperationDefinition: (node: any) => {
+      if (operationName && node.name.value != operationName) {
+        return null;
       }
-    };
-  
-    visit(ast, {leave:firstBuildVisitor});
-  
-    
-    ast = gql(print(visit(ast,{leave:firstRewriteVisitor})));
-  visit(ast,{leave:checkFragmentationVisitor});
-  
-  while(containsFrags){
-    containsFrags=false;
-    fragments={};
-    visit(ast, {enter:buildFragsVisitor});
-    
-    ast = gql(print(visit(ast,{leave:firstRewriteVisitor})));
-    visit(ast,{leave:checkFragmentationVisitor});
-    
+    },
+    InlineFragment: (node: any) => {
+      return [
+        {
+          kind: 'Field',
+          alias: undefined,
+          name: { kind: 'Name', value: '__typename' },
+          arguments: [],
+          directives: [],
+          selectionSet: undefined,
+        },
+        node,
+      ];
+    },
+  };
+
+  visit(ast, { leave: firstBuildVisitor });
+
+  ast = gql(print(visit(ast, { leave: firstRewriteVisitor })));
+  visit(ast, { leave: checkFragmentationVisitor });
+  while (containsFrags) {
+    containsFrags = false;
+    fragments = {};
+    visit(ast, { enter: buildFragsVisitor });
+
+    ast = gql(print(visit(ast, { leave: firstRewriteVisitor })));
+    visit(ast, { leave: checkFragmentationVisitor });
+
     //if existingFrags has a key that fragments does not
-    const exfragskeys=Object.keys(existingFrags);
-    const fragskeys=Object.keys(fragments);
-    const exvarsskeys=Object.keys(existingVars);
-    const varkeys =Object.keys(variables);
+    const exfragskeys = Object.keys(existingFrags);
+    const fragskeys = Object.keys(fragments);
+    const exvarsskeys = Object.keys(existingVars);
+    const varkeys = Object.keys(variables);
     //exfragskeys.every(key=>fragskeys.includes(key))
-    if (!exfragskeys.every(key=>fragskeys.includes(key))){
-      
-      return console.log({error: 'missing fragment definitions'})
+    if (!exfragskeys.every((key) => fragskeys.includes(key))) {
+      return console.log({ error: 'missing fragment definitions' });
     }
-    if (!exvarsskeys.every(key=>varkeys.includes(key))){
-      
-      return console.log({error: 'missing variable definitions'})
+    if (!exvarsskeys.every((key) => varkeys.includes(key))) {
+      return console.log({ error: 'missing variable definitions' });
     }
   }
 
-  
   ast = visit(ast, { leave: clearFragVisitor });
- 
+
   return print(ast);
 }
