06d35369dda36028dff7c773a3f2b112f34112e5
DavidMNorman <DavidMNorman@gmail.com>
2023-04-11 11:35:14 -0700

    Initial Test setup

---
+++ test_files/rhum_test_files/wTinyLFU_test.js
@@ -0,0 +1,5 @@
+import WTinyLFUCache from "../../src/Browser/wTinyLFUBrowserCache.js";
+import { Rhum } from 'https://deno.land/x/rhum@v1.1.11/mod.ts';
+import { test as testWrite } from '../test_variables/writeCache_variables.ts';
+import { test as testRead } from '../test_variables/readCache_variables.ts';
+
